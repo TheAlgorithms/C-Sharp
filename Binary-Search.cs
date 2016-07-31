00b2ce56c809ac6acd03b90d75e2f7941494392e
Harshil Darji <darjiharshil2994@gmail.com>
2016-07-31 22:21:35 +0530

    Removed unnecessary namespaces

--- Binary-Search.cs
+++ Binary-Search.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace BinarySearch_Iterative
+namespace Binary-Search
 {
     class Program
     {
