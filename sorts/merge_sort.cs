bef1e700928047fb9b0e4ab45d9b2993834091ee
Harshil Darji <darjiharshil2994@gmail.com>
2016-08-09 22:25:44 +0530

    Initial

---
+++ sorts/merge_sort.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace merge_sort
+{
+    class Program
+    {
+        static public void MainMerge(int[] numbers, int left, int mid, int right)
+        {
+            int[] temp = new int[25];
+            int i, eol, num, pos;
+
+            eol = (mid - 1);
+            pos = left;
+            num = (right - left + 1);
+
+            while ((left <= eol) && (mid <= right))
+            {
+                if (numbers[left] <= numbers[mid])
+                    temp[pos++] = numbers[left++];
+                else
+                    temp[pos++] = numbers[mid++];
+            }
+
+            while (left <= eol)
+                temp[pos++] = numbers[left++];
+
+            while (mid <= right)
+                temp[pos++] = numbers[mid++];
+
+            for (i = 0; i < num; i++)
+            {
+                numbers[right] = temp[right];
+                right--;
+            }
+        }
+
+        static public void SortMerge(int[] numbers, int left, int right)
+        {
+            int mid;
+
+            if (right > left)
+            {
+                mid = (right + left) / 2;
+                SortMerge(numbers, left, mid);
+                SortMerge(numbers, (mid + 1), right);
+
+                MainMerge(numbers, left, (mid + 1), right);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter some integers, separated by spaces:");
+            string input = Console.ReadLine();
+            string[] integers = input.Split(' ');
+            int[] data = new int[integers.Length];
+            for (int i = 0; i < data.Length; i++)
+                data[i] = int.Parse(integers[i]);
+
+            Console.Write("\nUnsorted: ");
+            for (int i = 0; i < data.Length; i++)
+            {
+                Console.Write(data[i] + " ");
+            }
+
+            SortMerge(data, 0, data.Length - 1);
+            Console.Write("\nSorted: ");
+            for (int i = 0; i < data.Length; i++)
+            {
+                Console.Write(data[i] + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
