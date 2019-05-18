9db1506171dd766bbcbdebf81a1ec1c05cdb0c43
siriak <siryaka@gmail.com>
2019-05-18 19:10:49 +0300

    Add VigenereEncoder

---
+++ Algorithms.Tests/Encoders/VigenereEncoderTests.cs
@@ -0,0 +1,27 @@
+﻿using Algorithms.Encoders;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace Algorithms.Tests.Encoders
+{
+    public class VigenereEncoderTests
+    {
+        readonly Randomizer random = new Randomizer();
+        readonly VigenereEncoder encoder = new VigenereEncoder();
+        string RandomMessage => random.GetString();
+
+        [Test]
+        [Parallelizable]
+        [Repeat(100)]
+        public void DecodedStringIsSame()
+        {
+            var message = RandomMessage;
+            var key = RandomMessage;
+
+            var encoded = encoder.Encode(message, key);
+            var decoded = encoder.Decode(encoded, key);
+
+            Assert.AreEqual(message, decoded);
+        }
+    }
+}
---
+++ Algorithms/Encoders/VigenereEncoder.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Text;
+
+namespace Algorithms.Encoders
+{
+    /// <summary>
+    /// Encodes using vigenere cypher
+    /// </summary>
+    public class VigenereEncoder : IEncoder<string>
+    {
+        readonly CaesarEncoder caesarEncoder = new CaesarEncoder();
+
+        /// <summary>
+        /// Encodes text using specified key
+        /// </summary>
+        /// <param name="text">Text to be encoded</param>
+        /// <param name="key">Key that will be used to encode the text</param>
+        /// <returns>Encoded text</returns>
+        public string Encode(string text, string key) => Cipher(text, key, caesarEncoder.Encode);
+
+        /// <summary>
+        /// Decodes text that was encoded using specified key
+        /// </summary>
+        /// <param name="text">Text to be decoded</param>
+        /// <param name="key">Key that was used to encode the text</param>
+        /// <returns>Decoded text</returns>
+        public string Decode(string text, string key) => Cipher(text, key, caesarEncoder.Decode);
+
+        private string Cipher(string text, string key, Func<string, int, string> symbolCipher)
+        {
+            key = AppendKey(key, text.Length);
+            var encodedTextBuilder = new StringBuilder(text.Length);
+            for (var i = 0; i < text.Length; i++)
+            {
+                if (!char.IsLetter(text[i]))
+                {
+                    encodedTextBuilder.Append(text[i]);
+                    continue;
+                }
+
+                var letterZ = char.IsUpper(key[i]) ? 'Z' : 'z';
+                var encodedSymbol = symbolCipher(text[i].ToString(), letterZ - key[i]);
+                encodedTextBuilder.Append(encodedSymbol);
+            }
+
+            return encodedTextBuilder.ToString();
+        }
+
+        private string AppendKey(string key, int length)
+        {
+            var keyBuilder = new StringBuilder(key, length);
+            while (keyBuilder.Length < length)
+            {
+                keyBuilder.Append(key);
+            }
+            return keyBuilder.ToString();
+        }
+    }
+}
