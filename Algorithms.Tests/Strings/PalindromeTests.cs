c0ce45061ed74cb9c1216ab76de403fdf399a6b4
ifisedi <51143882+1fisedi@users.noreply.github.com>
2019-06-16 08:13:57 -0500

    New palindrome check approach and tests. (#65)
    * New palindrome approach and tests.
    
    * Fix invariant culture

---
+++ Algorithms.Tests/Strings/PalindromeTests.cs
@@ -0,0 +1,32 @@
+﻿using Algorithms.Strings;
+using NUnit.Framework;
+
+namespace Algorithms.Tests.Strings
+{
+    public class PalindromeTests
+    {
+        [Test]
+        [Parallelizable]
+        public void InputStringIsPalindrome()
+        {
+            // Valid word
+            const string validWord = "Anna";
+            const string validPhrase = "A Santa at Nasa";
+            // Invalid word
+            const string invalidWord = "hallo";
+            const string invalidPhare = "Once upon a time";
+
+            // Act
+            var resultA = Palindrome.IsStringPalindrome(validWord);
+            var resultPhraseA = Palindrome.IsStringPalindrome(validPhrase);
+            var resultB = Palindrome.IsStringPalindrome(invalidWord);
+            var resultPhraseB = Palindrome.IsStringPalindrome(invalidPhare);
+
+            // Assert
+            Assert.True(resultA);
+            Assert.True(resultPhraseA);
+            Assert.False(resultB);
+            Assert.False(resultPhraseB);
+        }
+    }
+}
