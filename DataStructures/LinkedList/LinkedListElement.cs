df18c26b283e5c8da017dd0ba320bfbee57b89a3
S. Graf <39885965+Graflinger@users.noreply.github.com>
2019-06-23 21:09:26 +0200

    Development (#63)
    Add core structure for linked list, including ListElement and List
    Add the most relevant functions in class list
    Add tests functionality

---
+++ DataStructures/LinkedList/LinkedListElement.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures.LinkedList
+{
+    class LinkedListElement<T>
+    {
+        public T Data { get; set; }
+        public LinkedListElement<T> Next { get; set; }
+
+        public LinkedListElement(T data)
+        {
+            this.Data = data;
+            this.Next = null;
+        }
+
+        public override string ToString()
+        {
+            return ("Data: " + Data);
+        }
+    }
+}
+
+
