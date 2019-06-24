6db1f71323c2163b1b19cdf2d3c626fe78df34f1
Andrii Siriak <siryaka@gmail.com>
2019-06-24 07:40:41 +0300

    Refresh binary insertion sort (#69)
    * Refresh binary insertion sort
    
    * Add more descriptive variables

---
+++ Algorithms.Tests/Sorters/BinaryInsertionSorterTests.cs
@@ -0,0 +1,39 @@
+﻿using Algorithms.Sorters;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using System;
+using System.Collections.Generic;
+
+namespace Algorithms.Tests.Sorters
+{
+    public class BinaryInsertionSorterTests
+    {
+        private readonly BinaryInsertionSorter<int> sorter = new BinaryInsertionSorter<int>();
+        private readonly Random random = new Random();
+
+        [Test]
+        [Parallelizable]
+        public void ArraySorted([Random(0, 1000, 1000)]int n)
+        {
+            var testArray = new int[n];
+            var correctArray = new int[n];
+            for (var i = 0; i < n; i++)
+            {
+                var t = random.Next(0, 1000);
+                testArray[i] = t;
+                correctArray[i] = t;
+            }
+
+            var intComparer = new IntComparer();
+            sorter.Sort(testArray, intComparer);
+            Array.Sort(correctArray, intComparer);
+
+            Assert.AreEqual(testArray, correctArray);
+        }
+
+        private class IntComparer : IComparer<int>
+        {
+            public int Compare(int x, int y) => x.CompareTo(y);
+        }
+    }
+}
