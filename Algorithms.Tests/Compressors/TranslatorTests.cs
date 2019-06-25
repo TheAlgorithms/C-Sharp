bea9c03236f8f5cf6a3f940584104acd78915a9c
Andrii Siriak <siryaka@gmail.com>
2019-06-25 09:36:37 +0300

    Remove redundant parentheses

--- Algorithms.Tests/Compressors/TranslatorTests.cs
+++ Algorithms.Tests/Compressors/TranslatorTests.cs
@@ -11,7 +11,7 @@ namespace Algorithms.Tests.Compressors
         {
             // Arrange
             var translator = new Translator();
-            var dict = new Dictionary<string, string>()
+            var dict = new Dictionary<string, string>
             {
                 { "Hey", "Good day" },
                 { " ", " " },
