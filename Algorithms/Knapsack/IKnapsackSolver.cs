c87c3d72a166d92c8ffc68f1137b6830babf39a9
Andrii <siryaka@gmail.com>
2019-06-30 13:29:12 +0300

    Add knapsack problem

---
+++ Algorithms/Knapsack/IKnapsackSolver.cs
@@ -0,0 +1,12 @@
+﻿namespace Algorithms.Knapsack
+{
+    /// <summary>
+    /// Solves knapsack problem
+    /// Sum of values of taken items -> max
+    /// Sum of weights of taken items <= capacity
+    /// </summary>
+    /// <typeparam name="T">Type of items in knapsack</typeparam>
+    public interface IKnapsackSolver<T> : IHeuristicKnapsackSolver<T>
+    {
+    }
+}
