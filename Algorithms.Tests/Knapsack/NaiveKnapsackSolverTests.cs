179574e21d8aa5cb87f5fa998dd13894f3333aef
Andrii Siriak <siryaka@gmail.com>
2019-06-30 13:57:01 +0300

    Add knapsack problem (#74)
    * Add knapsack problem
    
    * Add knapsack problem tests
    
    * Add CRLF

---
+++ Algorithms.Tests/Knapsack/NaiveKnapsackSolverTests.cs
@@ -0,0 +1,27 @@
+﻿using Algorithms.Knapsack;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using System;
+using System.Linq;
+
+namespace Algorithms.Tests.Knapsack
+{
+    public class NaiveKnapsackSolverTests
+    {
+        [Test]
+        [Parallelizable]
+        public void TakesHalf([Random(0, 1000, 1000)]int length)
+        {
+            //Arrange
+            var solver = new NaiveKnapsackSolver<int>();
+            var items = Enumerable.Repeat(42, 2 * length).ToArray();
+            var expectedResult = Enumerable.Repeat(42, length);
+
+            //Act
+            var result = solver.Solve(items, length, x => 1, x => 1);
+
+            //Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
---
+++ Algorithms/Knapsack/NaiveKnapsackSolver.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithms.Knapsack
+{
+    /// <summary>
+    /// Greedy heurictic solver
+    /// </summary>
+    /// <typeparam name="T">Type of items in knapsack</typeparam>
+    public class NaiveKnapsackSolver<T> : IHeuristicKnapsackSolver<T>
+    {
+        public T[] Solve(T[] items, double capacity, Func<T, double> weightSelector, Func<T, double> valueSelector)
+        {
+            var weight = 0d;
+            var left = new List<T>();
+
+            foreach (var item in items)
+            {
+                var weightDelta = weightSelector(item);
+                if (weight + weightDelta <= capacity)
+                {
+                    weight += weightDelta;
+                    left.Add(item);
+                }
+            }
+
+            return left.ToArray();
+        }
+    }
+}
