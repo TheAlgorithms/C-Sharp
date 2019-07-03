2d3c771db4a699eb507e467bcac65d0a6e817e26
Andrii <siryaka@gmail.com>
2019-07-03 21:00:04 +0300

    Refresh BinaryGreatestCommonDivisorFinder

---
+++ Algorithms.Tests/Numeric/GreatestCommonDivisor/BinaryGreatestCommonDivisorFinderTests.cs
@@ -0,0 +1,31 @@
+﻿using Algorithms.Numeric.GreatestCommonDivisor;
+using NUnit.Framework;
+
+namespace Algorithms.Tests.Numeric.GreatestCommonDivisor
+{
+    public class BinaryGreatestCommonDivisorFinderTests
+    {
+        [Test]
+        [Parallelizable]
+        [TestCase(2, 3, 1)]
+        [TestCase(1, 1, 1)]
+        [TestCase(13, 17, 1)]
+        [TestCase(0, 17, 17)]
+        [TestCase(17, 0, 17)]
+        [TestCase(17, 17, 17)]
+        [TestCase(2 * 17, 17, 17)]
+        [TestCase(0, 0, int.MaxValue)]
+        [TestCase(2 * 13 * 17, 4 * 9 * 13, 2 * 13)]
+        public void GreatestCommonDivisorCorrect(int a, int b, int expectedGCD)
+        {
+            // Arrange
+            var gcdFinder = new BinaryGreatestCommonDivisorFinder();
+
+            // Act
+            var actualGCD = gcdFinder.Find(a, b);
+
+            // Assert
+            Assert.AreEqual(expectedGCD, actualGCD);
+        }
+    }
+}
