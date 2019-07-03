3434eea24873b6c9dec6c6cadf52982fd524b510
Andrii Siriak <siryaka@gmail.com>
2019-07-03 20:06:03 +0300

    Add IGreatestCommonDivisorFinder interface (#76)

---
+++ Algorithms/Numeric/GreatestCommonDivisor/IGreatestCommonDivisorFinder.cs
@@ -0,0 +1,13 @@
+﻿namespace Algorithms.Numeric.GreatestCommonDivisor
+{
+    public interface IGreatestCommonDivisorFinder
+    {
+        /// <summary>
+        /// Finds greatest common divisor for numbers a and b
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns>Greatest common divisor</returns>
+        int Find(int a, int b);
+    }
+}
