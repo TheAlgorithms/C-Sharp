071bce483c124ce2faaf016caa1ba44d55aea243
Andrii <siryaka@gmail.com>
2019-07-04 17:43:23 +0300

    Add comments

--- Algorithms/Search/ItemNotFoundException.cs
+++ Algorithms/Search/ItemNotFoundException.cs
@@ -2,6 +2,9 @@
 
 namespace Algorithms.Search
 {
+    /// <summary>
+    /// Signs that sequence doesn't contain any items that one was looking for
+    /// </summary>
     public class ItemNotFoundException : ApplicationException
     {
     }
--- Algorithms/Search/LinearSearcher.cs
+++ Algorithms/Search/LinearSearcher.cs
@@ -4,6 +4,14 @@ namespace Algorithms.Search
 {
     public class LinearSearcher<T> : ISearcher<T>
     {
+        /// <summary>
+        /// Finds first item in array that satisfies specified term
+        /// Time complexity: O(n)
+        /// Space complexity: O(1)
+        /// </summary>
+        /// <param name="data">Array to search in</param>
+        /// <param name="term">Term to check against</param>
+        /// <returns>First item that satisfies term</returns>
         public T Find(T[] data, Func<T, bool> term)
         {
             for (var i = 0; i < data.Length; i++)
@@ -16,6 +24,14 @@ namespace Algorithms.Search
             throw new ItemNotFoundException();
         }
 
+        /// <summary>
+        /// Finds index of first item in array that satisfies specified term
+        /// Time complexity: O(n)
+        /// Space complexity: O(1)
+        /// </summary>
+        /// <param name="data">Array to search in</param>
+        /// <param name="term">Term to check against</param>
+        /// <returns>Index of first item that satisfies term or -1 if none found</returns>
         public int FindIndex(T[] data, Func<T, bool> term)
         {
             for (var i = 0; i < data.Length; i++)
