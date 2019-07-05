1abd231fc59ba20da1d6312d2d64063697d08efd
Alina Siriak <adelhaide22@gmail.com>
2019-07-05 14:24:28 +0300

    Improve binary search

---
+++ Algorithms.Tests/Search/BinarySearcherTests.cs
@@ -0,0 +1,49 @@
+﻿using Algorithms.Searches;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Algorithms.Tests.Search
+{
+    public class BinarySearcherTests
+    {       
+        [Test]
+        [Parallelizable]
+        public void FindIndex_ItemPresent_IndexCorrect([Random(0, 1_000_000, 1_000)]int n)
+        {
+            // Arrange
+            var searcher = new BinarySearcher<int>();
+            var random = Randomizer.CreateRandomizer();
+            var arrayToSearch = Enumerable.Range(0, n).Select(x => random.Next(0, 1000)).OrderBy(x => x).ToArray();
+
+            // Act
+            var expectedIndex = Array.BinarySearch(arrayToSearch, arrayToSearch[n / 2]);
+            var actualIndex = searcher.FindIndex(arrayToSearch, arrayToSearch[n / 2]);
+
+            // Assert
+            Assert.AreEqual(expectedIndex, actualIndex);
+        }
+
+       
+        [Test]
+        [Parallelizable]
+        public void FindIndex_ItemMissing_MinusOneReturned([Random(0, 1_000_000, 1_000)]int n, [Random(-1_000, 1_001_000, 1_000)]int missingItem)
+        {
+            // Arrange
+            var searcher = new BinarySearcher<int>();
+            var random = Randomizer.CreateRandomizer();
+            var arrayToSearch = Enumerable.Range(0, n).Select(x => random.Next(0, 1000))
+                .Where(x => x != missingItem).OrderBy(x => x).ToArray();
+            var expectedIndex = -1;
+
+            // Act
+            var actualIndex = searcher.FindIndex(arrayToSearch, missingItem);
+
+            // Assert
+            Assert.AreEqual(expectedIndex, actualIndex);
+        }
+    }
+}
---
+++ Algorithms/Search/BinarySearcher.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Algorithms.Searches
+{
+    public class BinarySearcher<T> where T : IComparable<T>
+    {
+        /// <summary>
+        /// Finds index of first item in array that equals to item to search
+        /// Time complexity: O(logN)
+        /// Space complexity: O(1)
+        /// </summary>
+        /// <param name="sortedData">Sorted array to search in</param>
+        /// <param name="item">Item to search</param>
+        /// <returns>Index of first item that equals to item to search or -1 if none found</returns>
+        public int FindIndex(T[] sortedData, T item) 
+        {
+            var startIndex = 0;
+            var finishIndex = sortedData.Length - 1;
+
+            do
+            {
+                var middleIndex = startIndex + (finishIndex - startIndex) / 2;
+
+                if (item.CompareTo(sortedData[middleIndex]) == 1)
+                {
+                    startIndex = middleIndex + 1;
+                }
+                if (item.CompareTo(sortedData[middleIndex]) == -1)
+                {
+                    finishIndex = middleIndex - 1;
+                }
+                if (item.CompareTo(sortedData[middleIndex]) == 0)
+                {
+                    return middleIndex;
+                }
+            } while (startIndex <= finishIndex);
+
+            return -1;
+        }
+    }
+}
