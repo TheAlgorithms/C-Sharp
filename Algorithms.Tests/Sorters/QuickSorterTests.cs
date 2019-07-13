be9ff3700226ba3e1ff01e9ced5f1fb6a0c15c2f
Andrii Siriak <siryaka@gmail.com>
2019-07-13 14:28:33 +0300

    Refresh quicksort (#86)
    * Refresh quicksort
    
    * Update Algorithms.Tests/Sorters/QuickSorterTests.cs

---
+++ Algorithms.Tests/Sorters/QuickSorterTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Algorithms.Sorters;
+using Algorithms.Tests.Helpers;
+using NUnit.Framework;
+
+namespace Algorithms.Tests.Sorters
+{
+    public class QuickSorterTests
+    {
+        [Test]
+        public void ArraySorted([Random(0, 1000, 100, Distinct = true)]int n)
+        {
+            // Arrange
+            var sorter = new QuickSorter<int>();
+            var intComparer = new IntComparer();
+            var random = new Random();
+            var testArray = new int[n];
+            var correctArray = new int[n];
+            for (var i = 0; i < n; i++)
+            {
+                var t = random.Next(0, 1000);
+                testArray[i] = t;
+                correctArray[i] = t;
+            }
+
+            // Act
+            sorter.Sort(testArray, intComparer);
+            Array.Sort(correctArray, intComparer);
+
+            // Assert
+            Assert.AreEqual(testArray, correctArray);
+        }
+    }
+}
