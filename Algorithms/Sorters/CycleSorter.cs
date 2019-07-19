af2f606ef1221ee38f0f91bf587a6c00ca79ed2e
ifisedi <51143882+1fisedi@users.noreply.github.com>
2019-07-19 00:34:41 -0500

    Refresh merge sorter and add new tests (#93)
    * refresh merge sorter, cycle sorter and added new tests
    
    * fix test, add comparer

---
+++ Algorithms/Sorters/CycleSorter.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+
+namespace Algorithms.Sorters
+{
+    /// <summary>
+    /// Cycle sort is an in-place, unstable sorting algorithm,
+    /// a comparison sort that is theoretically optimal in terms of the total
+    /// number of writes to the original array.
+    /// It is based on the idea that the permutation to be sorted can be factored
+    /// into cycles, which can individually be rotated to give a sorted result.
+    /// </summary>
+    /// <typeparam name="T">Type array input.</typeparam>
+    public class CycleSorter<T> : ISorter<T>
+    {
+        /// <summary>
+        /// Sorts input array using Cycle sort.
+        /// </summary>
+        /// <param name="array">Input array.</param>
+        /// <param name="comparer">Integer comparer.</param>
+        public void Sort(T[] array, IComparer<T> comparer) => CycleSort(array, comparer);
+
+        private static void CycleSort(IList<T> data, IComparer<T> comparer)
+        {
+            for (var cycleStart = 0; cycleStart <= data.Count - 2; cycleStart++)
+            {
+                var item = data[cycleStart];
+                var pos = cycleStart;
+
+                for (var i = cycleStart + 1; i <= data.Count - 1; i++)
+                {
+                    if (comparer.Compare(data[i], item) == -1)
+                    {
+                        pos++;
+                    }
+                }
+
+                if (pos == cycleStart)
+                {
+                    continue;
+                }
+
+                while (comparer.Compare(data[pos], item) == 0)
+                {
+                    pos++;
+                }
+
+                var temp = data[pos];
+                data[pos] = item;
+                item = temp;
+
+                while (pos != cycleStart)
+                {
+                    pos = cycleStart;
+                    for (var i = cycleStart + 1; i <= data.Count - 1; i++)
+                    {
+                        if (comparer.Compare(data[i], item) == -1)
+                        {
+                            pos++;
+                        }
+                    }
+
+                    while (comparer.Compare(data[pos], item) == 0)
+                    {
+                        pos++;
+                    }
+
+                    temp = data[pos];
+                    data[pos] = item;
+                    item = temp;
+                }
+            }
+        }
+    }
+}
---
+++ Algorithms/Sorters/MergeSorter.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+
+namespace Algorithms.Sorters
+{
+
+    /// <summary>
+    /// Divide and Conquer algorithm, which splits#
+    /// input array in two halves, calls itself for the two halves and
+    /// then merges the two sorted halves.
+    /// </summary>
+    /// <typeparam name="T">type of array</typeparam>
+    public class MergeSorter<T> : ISorter<T>
+    {
+        /// <summary>
+        /// Sorts array using merge algorithm.
+        /// </summary>
+        /// <param name="array">Input integer array.</param>
+        /// <param name="comparer">Comparer.</param>
+        public void Sort(T[] array, IComparer<T> comparer) => SortMerge(array, 0, array.Length - 1, comparer);
+
+        private static void MainMerge(IList<T> numbers, int left, int mid, int right, IComparer<T> comparer)
+        {
+            var temp = new T[numbers.Count];
+
+            int i;
+
+            var eol = mid - 1;
+            var pos = left;
+            var num = right - left + 1;
+
+            while (left <= eol && mid <= right)
+            {
+                var compResult = comparer.Compare(numbers[left], numbers[mid]);
+                temp[pos++] = compResult <= 0 ? numbers[left++] : numbers[mid++];
+            }
+
+            while (left <= eol)
+            {
+                temp[pos++] = numbers[left++];
+            }
+
+            while (mid <= right)
+            {
+                temp[pos++] = numbers[mid++];
+            }
+
+            for (i = 0; i < num; i++)
+            {
+                numbers[right] = temp[right];
+                right--;
+            }
+        }
+
+        private static void SortMerge(IList<T> numbers, int left, int right, IComparer<T> comparer)
+        {
+            if (right <= left)
+            {
+                return;
+            }
+
+            var mid = left + (right - left) / 2;
+            SortMerge(numbers, left, mid, comparer);
+            SortMerge(numbers, mid + 1, right, comparer);
+            MainMerge(numbers, left, mid + 1, right, comparer);
+        }
+    }
+}
