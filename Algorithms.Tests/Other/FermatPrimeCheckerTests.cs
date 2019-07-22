c91117d411a9a54f0ff90a50bc67ac4915bb3423
1fisedi <51143882+1fisedi@users.noreply.github.com>
2019-07-21 20:28:39 -0500

    Refresh on Fermat prime checker and added new tests.

---
+++ Algorithms.Tests/Other/FermatPrimeCheckerTests.cs
@@ -0,0 +1,29 @@
+﻿using Algorithms.Other;
+
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace Algorithms.Tests.Other
+{
+    public static class FermatPrimeCheckerTests
+    {
+        [Test]
+        [TestCase(5, true)]
+        [TestCase(2633, true)]
+        [TestCase(9439, true)]
+        [TestCase(1, false)]
+        [TestCase(8, false)]
+        public static void IsProbablePrime(int inputNum, bool expected)
+        {
+            // Arrange
+            var random = new Randomizer();
+            var times = random.Next(1, 1000);
+
+            // Act
+            var result = FermatPrimeChecker.IsPrime(inputNum, times);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
