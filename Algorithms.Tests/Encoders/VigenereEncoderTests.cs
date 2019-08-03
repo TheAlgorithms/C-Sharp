22b78a11d1c3561b98af5e60fdecc1b2e6efb78a
1fisedi <51143882+1fisedi@users.noreply.github.com>
2019-08-03 12:11:37 -0500

    removed commented code

--- Algorithms.Tests/Encoders/VigenereEncoderTests.cs
+++ Algorithms.Tests/Encoders/VigenereEncoderTests.cs
@@ -23,22 +23,5 @@ namespace Algorithms.Tests.Encoders
             // Assert
             Assert.AreEqual(message, decoded);
         }
-
-        //[Test]
-        //public void DecodedStringIsTheSameManualKey()
-        //{
-        //    // Arrange
-        //    var random = new Randomizer();
-        //    var encoder = new VigenereEncoder();
-        //    var message = random.GetString();
-        //    const string key = "shiftIt";
-
-        //    // Act
-        //    var encoded = encoder.Encode(message, key);
-        //    var decoded = encoder.Decode(encoded, key);
-
-        //    // Assert
-        //    Assert.AreEqual(message, decoded);
-        //}
     }
 }
