836dbdf97b779b8d2c31093f68bf5994413488db
Barry Stahl <bss_github@bsstahl.com>
2019-08-08 22:14:51 -0700

    Setup DPKnapsack Solver solution and tests

---
+++ Algorithms.Tests/Knapsack/DPKnapsackSolverTests.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+using Algorithms.Knapsack;
+using NUnit.Framework;
+
+namespace Algorithms.Tests.Knapsack
+{
+    public static class DPKnapsackSolverTests
+    {
+
+        [Test]
+        public static void SmallSample()
+        {
+            //Arrange
+            var items = new int[] { 0, 1, 2 };
+            var val = new int[] { 50, 100, 130 };
+            var wt = new int[] { 10, 20, 40 };
+            int capacity = 50;
+            var expected = new int[] { 0, 2 };
+
+            //Act
+            var solver = new DPKnapsackSolver<int>();
+            var actual = solver.Solve(items, capacity, x => wt[x], y => val[y]);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public static void TakesHalf([Random(0, 1000, 100, Distinct = true)]int length)
+        {
+            //Arrange
+            var solver = new DPKnapsackSolver<int>();
+            var items = Enumerable.Repeat(42, 2 * length).ToArray();
+            var expectedResult = Enumerable.Repeat(42, length);
+
+            //Act
+            var result = solver.Solve(items, length, x => 1, y => 1);
+
+            //Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
---
+++ Algorithms/Knapsack/DPKnapsackSolver.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Knapsack
+{
+    /// <summary>
+    /// Dynamic Programming Knapsack solver.
+    /// </summary>
+    /// <typeparam name="T">Type of items in knapsack.</typeparam>
+    public class DPKnapsackSolver<T> : IHeuristicKnapsackSolver<T>
+    {
+        /// <summary>
+        /// Returns the optimal weights for items in the knapsack
+        /// so as to maximize value while not exceeding capacity.
+        /// </summary>
+        /// <param name="items">TODO. 2.</param>
+        /// <param name="capacity">TODO. 3.</param>
+        /// <param name="weightSelector">TODO. 4.</param>
+        /// <param name="valueSelector">TODO. 5.</param>
+        /// <returns>TODO. 6.</returns>
+        public T[] Solve(T[] items, double capacity, Func<T, double> weightSelector, Func<T, double> valueSelector)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
