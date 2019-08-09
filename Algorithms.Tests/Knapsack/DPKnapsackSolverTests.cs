feca41af0a941ed56145b328451d6a15a1651256
Barry Stahl <bss_github@bsstahl.com>
2019-08-09 11:00:51 -0700

    Solves problems where weights and values are integral but solves them incorrectly if the values include non-integers.

--- Algorithms.Tests/Knapsack/DPKnapsackSolverTests.cs
+++ Algorithms.Tests/Knapsack/DPKnapsackSolverTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using Algorithms.Knapsack;
 using NUnit.Framework;
 
@@ -8,22 +9,108 @@ namespace Algorithms.Tests.Knapsack
     {
 
         [Test]
-        public static void SmallSample()
+        public static void SmallSampleOfChar()
         {
             //Arrange
-            var items = new int[] { 0, 1, 2 };
+            var items = new char[] { 'A', 'B', 'C' };
             var val = new int[] { 50, 100, 130 };
             var wt = new int[] { 10, 20, 40 };
+
             int capacity = 50;
-            var expected = new int[] { 0, 2 };
+
+            Func<char, double> weightSelector = x => wt[Array.IndexOf(items, x)];
+            Func<char, double> valueSelector = x => val[Array.IndexOf(items, x)];
+
+            var expected = new char[] { 'A', 'C' };
+
 
             //Act
-            var solver = new DPKnapsackSolver<int>();
-            var actual = solver.Solve(items, capacity, x => wt[x], y => val[y]);
+            var solver = new DPKnapsackSolver<char>();
+            var actual = solver.Solve(items, capacity, weightSelector, valueSelector);
 
             //Assert
             Assert.AreEqual(expected, actual);
         }
+
+        [Test]
+        public static void FSU_P01()
+        {
+            // Data from https://people.sc.fsu.edu/~jburkardt/datasets/knapsack_01/knapsack_01.html
+
+            //Arrange
+            var items = new char[] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J' };
+            var val = new int[] { 92, 57, 49, 68, 60, 43, 67, 84, 87, 72 };
+            var wt = new int[] { 23, 31, 29, 44, 53, 38, 63, 85, 89, 82 };
+
+            int capacity = 165;
+
+            Func<char, double> weightSelector = x => wt[Array.IndexOf(items, x)];
+            Func<char, double> valueSelector = x => val[Array.IndexOf(items, x)];
+
+            var expected = new char[] { 'A', 'B', 'C', 'D', 'F' };
+
+
+            //Act
+            var solver = new DPKnapsackSolver<char>();
+            var actual = solver.Solve(items, capacity, weightSelector, valueSelector);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public static void FSU_P07()
+        {
+            // Data from https://people.sc.fsu.edu/~jburkardt/datasets/knapsack_01/knapsack_01.html
+
+            //Arrange
+            var val = new int[] { 135, 139, 149, 150, 156, 163, 173, 184, 192, 201, 210, 214, 221, 229, 240 };
+            var wt = new int[] { 70, 73, 77, 80, 82, 87, 90, 94, 98, 106, 110, 113, 115, 118, 120 };
+            var items = Enumerable.Range(1, val.Count()).ToArray();
+
+            int capacity = 750;
+
+            Func<int, double> weightSelector = x => wt[Array.IndexOf(items, x)];
+            Func<int, double> valueSelector = x => val[Array.IndexOf(items, x)];
+
+            var expected = new int[] { 1, 3, 5, 7, 8, 9, 14, 15 };
+
+
+            //Act
+            var solver = new DPKnapsackSolver<int>();
+            var actual = solver.Solve(items, capacity, weightSelector, valueSelector);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [Test]
+        public static void FSU_P07_AsDouble()
+        {
+            // Data from https://people.sc.fsu.edu/~jburkardt/datasets/knapsack_01/knapsack_01.html
+
+            //Arrange
+            var val = new double[] { 13.5, 13.9, 14.9, 15.0, 15.6, 16.3, 17.3, 18.4, 19.2, 20.1, 21.0, 21.4, 22.1, 22.9, 24.0 };
+            var wt = new double[] { 7.0, 7.3, 7.7, 8.0, 8.2, 8.7, 9.0, 9.4, 9.8, 10.6, 11.0, 11.3, 11.5, 11.8, 12.0 };
+            var items = Enumerable.Range(1, val.Count()).ToArray();
+
+            double capacity = 75.0;
+
+            Func<int, double> weightSelector = x => wt[Array.IndexOf(items, x)];
+            Func<int, double> valueSelector = x => val[Array.IndexOf(items, x)];
+
+            var expected = new int[] { 1, 3, 5, 7, 8, 9, 14, 15 };
+
+
+            //Act
+            var solver = new DPKnapsackSolver<int>();
+            var actual = solver.Solve(items, capacity, weightSelector, valueSelector);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
 
         [Test]
         public static void TakesHalf([Random(0, 1000, 100, Distinct = true)]int length)
--- Algorithms/Knapsack/DPKnapsackSolver.cs
+++ Algorithms/Knapsack/DPKnapsackSolver.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Algorithms.Knapsack
@@ -10,18 +11,117 @@ namespace Algorithms.Knapsack
     /// <typeparam name="T">Type of items in knapsack.</typeparam>
     public class DPKnapsackSolver<T> : IHeuristicKnapsackSolver<T>
     {
+        private readonly int maxTableSize;
+
         /// <summary>
-        /// Returns the optimal weights for items in the knapsack
-        /// so as to maximize value while not exceeding capacity.
+        /// Initializes a new instance of the <see cref="DPKnapsackSolver{T}"/> class.
         /// </summary>
-        /// <param name="items">TODO. 2.</param>
-        /// <param name="capacity">TODO. 3.</param>
-        /// <param name="weightSelector">TODO. 4.</param>
-        /// <param name="valueSelector">TODO. 5.</param>
-        /// <returns>TODO. 6.</returns>
+        /// <param name="maxTableSize">Limits the total amount of memory used by
+        /// specifying the maximum size of the memoization table. The size of this
+        /// table is calculated by multiplying 1 plus the number of items that can be
+        /// selected by the maximum capacity of the knapsack and the desired
+        /// granularity of that weight.</param>
+        /// <example>If 9 items can chosen to fit into a knapsack of
+        /// capacity 50.2 and we want the weight to be calculated to 1 decimal
+        /// place, then the table size would be 5000 (10 * 500).</example>
+        public DPKnapsackSolver(int maxTableSize = 10000000)
+        {
+            this.maxTableSize = maxTableSize;
+        }
+
+        /// <summary>
+        /// Returns the knapsack containing the items that
+        /// maximize value while not exceeding weight capacity.
+        /// </summary>
+        /// <param name="items">The list of items from which we select ones to be in the knapsack.</param>
+        /// <param name="capacity">The maximum weight capacity of the knapsack to be filled.</param>
+        /// <param name="weightSelector">A function that returns the value of the specified item
+        /// from the <paramref name="items">items</paramref> list.</param>
+        /// <param name="valueSelector">A function that returns the weight of the specified item
+        /// from the <paramref name="items">items</paramref> list.</param>
+        /// <returns>The array of items that provides the maximum value of the
+        /// knapsack without exceeding the specified weight <paramref name="capacity">capacity</paramref>.</returns>
         public T[] Solve(T[] items, double capacity, Func<T, double> weightSelector, Func<T, double> valueSelector)
         {
-            throw new NotImplementedException();
+            if (DetermineTableSize(capacity, items.Count()) > this.maxTableSize)
+            {
+                throw new InvalidOperationException($"{items.Count()} items with a knapsack capacity of {capacity} would result in a memoization table size larger than the configured value of {this.maxTableSize}.");
+            }
+
+            int decimalPlacesToAdjustWeight = CalculateDecimalPlaceAccuracy(this.maxTableSize, items.Count());
+
+            int maxCapacity = Convert.ToInt32(Math.Ceiling(capacity * Math.Pow(10, decimalPlacesToAdjustWeight)));
+            Func<T, int> adjustedWeightSelector = x => Convert.ToInt32(Math.Round(weightSelector(x) * Math.Pow(10, decimalPlacesToAdjustWeight)));
+
+            var memoTable = Memoize(items, adjustedWeightSelector, valueSelector, maxCapacity);
+            return GetOptimalItems(items, adjustedWeightSelector, memoTable, maxCapacity);
+        }
+
+        private static int DetermineTableSize(double capacity, int itemCount)
+        {
+            return Convert.ToInt32(Math.Ceiling(capacity)) * (itemCount + 1);
+        }
+
+        private static int CalculateDecimalPlaceAccuracy(int maxTableSize, int numberOfItems)
+        {
+            // TODO: Implement for real
+            return 0;
+        }
+
+        private static T[] GetOptimalItems(T[] items, Func<T, int> weightSelector, double[,] memoTable, int capacity)
+        {
+            int currentCapacity = capacity;
+
+            var result = new List<T>();
+            for (int i = items.Count() - 1; i >= 0; i--)
+            {
+                if (memoTable[i + 1, currentCapacity] > memoTable[i, currentCapacity])
+                {
+                    var item = items[i];
+                    result.Add(item);
+                    currentCapacity -= weightSelector(item);
+                }
+            }
+
+            result.Reverse(); // we added items back to front
+            return result.ToArray();
+        }
+
+        private static double[,] Memoize(T[] items, Func<T, int> weightSelector, Func<T, double> valueSelector, int maxCapacity)
+        {
+            // Memoize in a bottom up manner
+            int n = items.Count();
+            var rv = new double[n + 1, maxCapacity + 1];
+            for (var i = 0; i <= n; i++)
+            {
+                for (var w = 0; w <= maxCapacity; w++)
+                {
+                    if (i == 0 || w == 0)
+                    {
+                        // If we have no items to take, or
+                        // if we have no capacity in our knapsack
+                        // we cannot possibly have any value
+                        rv[i, w] = 0;
+                    }
+                    else if (weightSelector(items[i - 1]) <= w)
+                    {
+                        // Decide if it is better to take or not take this item
+                        var iut = items[i - 1]; // iut = Item under test
+                        var vut = valueSelector(iut); // vut = Value of item under test
+                        var wut = weightSelector(iut); // wut = Weight of item under test
+                        var valueIfTaken = vut + rv[i - 1, w - wut];
+                        var valueIfNotTaken = rv[i - 1, w];
+                        rv[i, w] = Math.Max(valueIfTaken, valueIfNotTaken);
+                    }
+                    else
+                    {
+                        // There is not enough room to take this item
+                        rv[i, w] = rv[i - 1, w];
+                    }
+                }
+            }
+
+            return rv;
         }
     }
 }
