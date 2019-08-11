63f6582dcf3806e9e88d3f2400e0e60859066f85
Barry Stahl <bss_github@bsstahl.com>
2019-08-11 01:40:22 -0500

    Add C-Sharp Dynamic Programming Knapsack Solver (#110)
    * Setup DPKnapsack Solver solution and tests
    
    * Solves problems where weights and values are integral but solves them incorrectly if the values include non-integers.
    
    * Rounds capacity down and all items up when using them to handle double values as required by the interface definition.
    
    * Update ReadMe to add link to DP Knapsack algorithm.
    
    * Update to fix style issues reported by codacy
    
    * Fixed 2 Codacy style issues in the unit test class
    
    * Clean-up language describing how to handle a need for greater resolution in the method docs
    
    Co-Authored-By: Andrii Siriak <siryaka@gmail.com>
    
    * Use Array property Length rather than Linq extension method Count().
    
    Co-Authored-By: Andrii Siriak <siryaka@gmail.com>
    
    * Use Array native property Length instead of Linq extension Count().
    
    I tend to use Enumerables instead of arrays so I default to using the Count() method but when using Arrays, Length is more appropriate and probably more efficient.
    
    Co-Authored-By: Andrii Siriak <siryaka@gmail.com>
    
    * Update README.md to reflect modified name of the Dynamic Programming Solver
    
    Co-Authored-By: Andrii Siriak <siryaka@gmail.com>
    
    * Remove dependency on order in tests in case future implementations use Enumerables instead of Arrays. Other minor fixes to wording and to prefer using the Length property of Array rather than the linq extension method Count().
    
    * Make weights integer

---
+++ Algorithms.Tests/Knapsack/DynamicProgrammingKnapsackSolverTests.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Linq;
+using Algorithms.Knapsack;
+using NUnit.Framework;
+
+namespace Algorithms.Tests.Knapsack
+{
+    public static class DynamicProgrammingKnapsackSolverTests
+    {
+
+        [Test]
+        public static void SmallSampleOfChar()
+        {
+            //Arrange
+            var items = new [] { 'A', 'B', 'C' };
+            var val = new [] { 50, 100, 130 };
+            var wt = new [] { 10, 20, 40 };
+
+            var capacity = 50;
+
+            Func<char, int> weightSelector = x => wt[Array.IndexOf(items, x)];
+            Func<char, double> valueSelector = x => val[Array.IndexOf(items, x)];
+
+            var expected = new [] { 'A', 'C' };
+
+
+            //Act
+            var solver = new DynamicProgrammingKnapsackSolver<char>();
+            var actual = solver.Solve(items, capacity, weightSelector, valueSelector);
+
+            //Assert
+            Assert.AreEqual(expected.OrderBy(x => x), actual.OrderBy(x => x));
+        }
+
+        [Test]
+        public static void FSU_P01()
+        {
+            // Data from https://people.sc.fsu.edu/~jburkardt/datasets/knapsack_01/knapsack_01.html
+
+            //Arrange
+            var items = new [] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J' };
+            var val = new [] { 92, 57, 49, 68, 60, 43, 67, 84, 87, 72 };
+            var wt = new [] { 23, 31, 29, 44, 53, 38, 63, 85, 89, 82 };
+
+            var capacity = 165;
+
+            Func<char, int> weightSelector = x => wt[Array.IndexOf(items, x)];
+            Func<char, double> valueSelector = x => val[Array.IndexOf(items, x)];
+
+            var expected = new [] { 'A', 'B', 'C', 'D', 'F' };
+
+            //Act
+            var solver = new DynamicProgrammingKnapsackSolver<char>();
+            var actual = solver.Solve(items, capacity, weightSelector, valueSelector);
+
+            //Assert
+            Assert.AreEqual(expected.OrderBy(x => x), actual.OrderBy(x => x));
+        }
+
+        [Test]
+        public static void FSU_P07_WithNonIntegralValues()
+        {
+            // Shows how to handle weights with 1 significant digit right of the decimal
+            // Data from https://people.sc.fsu.edu/~jburkardt/datasets/knapsack_01/knapsack_01.html
+
+            //Arrange
+            var val = new [] { 135, 139, 149, 150, 156, 163, 173, 184, 192, 201, 210, 214, 221, 229, 240 };
+            var wt = new [] { 7.0, 7.3, 7.7, 8.0, 8.2, 8.7, 9.0, 9.4, 9.8, 10.6, 11.0, 11.3, 11.5, 11.8, 12.0 };
+            var items = Enumerable.Range(1, val.Count()).ToArray();
+
+            var capacity = 75;
+
+            Func<int, int> weightSelector = x => (int)(wt[Array.IndexOf(items, x)] * 10);
+            Func<int, double> valueSelector = x => val[Array.IndexOf(items, x)];
+
+            var expected = new [] { 1, 3, 5, 7, 8, 9, 14, 15 };
+
+
+            //Act
+            var solver = new DynamicProgrammingKnapsackSolver<int>();
+            var actual = solver.Solve(items, capacity * 10, weightSelector, valueSelector);
+
+            //Assert
+            Assert.AreEqual(expected.OrderBy(x => x), actual.OrderBy(x => x));
+        }
+
+
+        [Test]
+        public static void TakesHalf([Random(0, 1000, 100, Distinct = true)]int length)
+        {
+            //Arrange
+            var solver = new DynamicProgrammingKnapsackSolver<int>();
+            var items = Enumerable.Repeat(42, 2 * length).ToArray();
+            var expectedResult = Enumerable.Repeat(42, length);
+
+            //Act
+            var result = solver.Solve(items, length, x => 1, y => 1);
+
+            //Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
---
+++ Algorithms/Knapsack/DynamicProgrammingKnapsackSolver.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithms.Knapsack
+{
+    /// <summary>
+    /// Dynamic Programming Knapsack solver.
+    /// </summary>
+    /// <typeparam name="T">Type of items in knapsack.</typeparam>
+    public class DynamicProgrammingKnapsackSolver<T>
+    {
+        /// <summary>
+        /// Returns the knapsack containing the items that
+        /// maximize value while not exceeding weight capacity.
+        /// </summary>
+        /// <param name="items">The list of items from which we select ones to be in the knapsack.</param>
+        /// <param name="capacity">The maximum weight capacity of the knapsack
+        /// to be filled. Only integer values of this capacity are tried. If
+        /// a greater resolution is needed, multiply the
+        /// weights/capacity by a factor of 10.</param>
+        /// <param name="weightSelector">A function that returns the value of the specified item
+        /// from the <paramref name="items">items</paramref> list.</param>
+        /// <param name="valueSelector">A function that returns the weight of the specified item
+        /// from the <paramref name="items">items</paramref> list.</param>
+        /// <returns>The array of items that provides the maximum value of the
+        /// knapsack without exceeding the specified weight <paramref name="capacity">capacity</paramref>.</returns>
+        public T[] Solve(T[] items, int capacity, Func<T, int> weightSelector, Func<T, double> valueSelector)
+        {
+            var cache = Tabulate(items, weightSelector, valueSelector, capacity);
+            return GetOptimalItems(items, weightSelector, cache, capacity);
+        }
+
+        private static T[] GetOptimalItems(T[] items, Func<T, int> weightSelector, double[,] cache, int capacity)
+        {
+            var currentCapacity = capacity;
+
+            var result = new List<T>();
+            for (var i = items.Length - 1; i >= 0; i--)
+            {
+                if (cache[i + 1, currentCapacity] > cache[i, currentCapacity])
+                {
+                    var item = items[i];
+                    result.Add(item);
+                    currentCapacity -= weightSelector(item);
+                }
+            }
+
+            result.Reverse(); // we added items back to front
+            return result.ToArray();
+        }
+
+        private static double[,] Tabulate(T[] items, Func<T, int> weightSelector, Func<T, double> valueSelector, int maxCapacity)
+        {
+            // Store the incremental results in a bottom up manner
+            var n = items.Length;
+            var results = new double[n + 1, maxCapacity + 1];
+            for (var i = 0; i <= n; i++)
+            {
+                for (var w = 0; w <= maxCapacity; w++)
+                {
+                    if (i == 0 || w == 0)
+                    {
+                        // If we have no items to take, or
+                        // if we have no capacity in our knapsack
+                        // we cannot possibly have any value
+                        results[i, w] = 0;
+                    }
+                    else if (weightSelector(items[i - 1]) <= w)
+                    {
+                        // Decide if it is better to take or not take this item
+                        var iut = items[i - 1]; // iut = Item under test
+                        var vut = valueSelector(iut); // vut = Value of item under test
+                        var wut = weightSelector(iut); // wut = Weight of item under test
+                        var valueIfTaken = vut + results[i - 1, w - wut];
+                        var valueIfNotTaken = results[i - 1, w];
+                        results[i, w] = Math.Max(valueIfTaken, valueIfNotTaken);
+                    }
+                    else
+                    {
+                        // There is not enough room to take this item
+                        results[i, w] = results[i - 1, w];
+                    }
+                }
+            }
+
+            return results;
+        }
+    }
+}
