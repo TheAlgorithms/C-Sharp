9e9bb0a846525c1437ae3bf8d52cf6eb71485721
MatanDavidi <davidi.matan@gmail.com>
2019-10-14 22:20:25 +0200

    Change Fibonacci class to use 'yield' statements and iterators

--- Algorithms/Numeric/Fibonacci.cs
+++ Algorithms/Numeric/Fibonacci.cs
@@ -8,34 +8,29 @@ namespace Algorithms.Numeric
     /// The class Fibonacci handles the generation and printing of the fibonacci sequence up to its nth element.
     /// For additional information, see https://en.wikipedia.org/wiki/Fibonacci_number.
     /// </summary>
+    /// <summary>
+    /// The class Fibonacci handles the generation of the fibonacci sequence.
+    /// For additional information, see https://en.wikipedia.org/wiki/Fibonacci_number.
+    /// </summary>
     public abstract class Fibonacci
     {
         /// <summary>
-        /// Generates and returns the first n elements of the Fibonacci sequence.
+        /// Yields the Fibonacci sequence.
         /// </summary>
-        /// <param name="size">The number of elements in the sequence to generate.</param>
-        /// <returns>An IEnumerable of ulong variables containing the first elements of the Fibonacci sequence.</returns>
-        /// <exception cref="ArgumentOutOfRangeException">This exception is thrown if a negative size is specified.</exception>
-        public static IEnumerable<ulong> GetSequence(int size)
+        /// <returns>An iterator that contains the Fibonacci sequence.</returns>
+        public static IEnumerable<ulong> GetSequence()
         {
-            List<ulong> sequence = new List<ulong>(size);
-            if (size >= 2)
+            yield return 0;
+            yield return 1;
+            ulong a = 0;
+            ulong b = 1;
+            while (true)
             {
-                sequence.Add(0);
-                sequence.Add(1);
-                ulong a = 0;
-                ulong b = 1;
-                for (int i = 2; i < size; ++i)
-                {
-                    sequence.Add(a + b);
-
-                    ulong temp = a + b;
-                    a = b;
-                    b = temp;
-                }
+                yield return a + b;
+                ulong temp = a + b;
+                a = b;
+                b = temp;
             }
-
-            return sequence;
         }
     }
 }
