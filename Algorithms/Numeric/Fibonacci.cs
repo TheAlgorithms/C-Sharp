0ca98aa9858049196065e94e10e1e303b2963f54
Matan Davidi <davidi.matan@gmail.com>
2019-10-16 08:50:22 +0200

    Add Fibonacci sequence (#116)
    * Add Fibonacci algorithm class
    
    * Add Fibonacci algorithm to README.md

---
+++ Algorithms/Numeric/Fibonacci.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Numeric
+{
+    /// <summary>
+    /// The class Fibonacci handles the generation and printing of the fibonacci sequence up to its nth element.
+    /// For additional information, see https://en.wikipedia.org/wiki/Fibonacci_number.
+    /// </summary>
+    public static class Fibonacci
+    {
+        /// <summary>
+        /// Yields the Fibonacci sequence.
+        /// </summary>
+        /// <returns>An iterator that contains the Fibonacci sequence.</returns>
+        public static IEnumerable<ulong> GetSequence()
+        {
+            yield return 0;
+            yield return 1;
+            ulong a = 0;
+            ulong b = 1;
+            while (true)
+            {
+                var temp = a + b;
+                yield return temp;
+                a = b;
+                b = temp;
+            }
+        }
+    }
+}
