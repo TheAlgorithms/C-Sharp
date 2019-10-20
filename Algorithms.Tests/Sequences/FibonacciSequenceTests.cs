3404cc44cba7ae3a4eda621f8c51e3f71dfed4cd
Andrii Siriak <siryaka@gmail.com>
2019-10-20 10:59:29 +0300

    Move Fibonacci sequence to separate namespace

---
+++ Algorithms.Tests/Sequences/FibonacciSequenceTests.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using System.Numerics;
+using Algorithms.Sequences;
+using NUnit.Framework;
+
+namespace Algorithms.Tests.Sequences
+{
+    public class FibonacciSequenceTests
+    {
+        [Test]
+        public void First10ElementsCorrect()
+        {
+            var sequence = new FibonacciSequence().Sequence;
+
+            Assert.AreEqual(new BigInteger[] { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34 }, sequence.Take(10));
+        }
+    }
+}
---
+++ Algorithms/Sequences/ISequence.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Numerics;
+
+namespace Algorithms.Sequences
+{
+    /// <summary>
+    /// Common interface for all integer sequences.
+    /// </summary>
+    public interface ISequence
+    {
+        /// <summary>
+        /// Gets sequence as enumerable.
+        /// </summary>
+        IEnumerable<BigInteger> Sequence { get; }
+    }
+}
