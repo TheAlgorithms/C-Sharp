a72fa9c3b2751765b4381ed91b4a7dea58c7d43b
Witteborn <Witteborn@posteo.de>
2019-12-04 16:38:58 +0100

    Add tests for FastSearch algorithm

---
+++ Algorithms.Tests/Search/FastSearcherTests.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Linq;
+using Algorithms.Search;
+
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace Algorithms.Tests.Search
+{
+    public static class FastSearcherTests
+    {
+
+        [Test]
+        public static void FindIndex_ItemPresent_IndexCorrect()
+        {
+            // Arrange
+            var searcher = new FastSearcher();
+            var arr = new int[100000];
+            for (var i = 0; i < arr.Length; i++)
+            {
+                arr[i] = i;
+            }
+
+            Array.Sort(arr);
+
+            const int expectedIndex = 3;
+            var x = arr[expectedIndex];
+            var actualIndex = searcher.FindIndex(arr, x);
+
+            // Assert
+            Assert.AreEqual(expectedIndex, actualIndex);
+        }
+
+        [Test]
+        public static void FindIndex_ItemPresent_IndexCorrect_ArrayIsOdd()
+        {
+            // Arrange
+            var searcher = new FastSearcher();
+            var arr = new int[100001];
+            for (var i = 0; i < arr.Length; i++)
+            {
+                arr[i] = i;
+            }
+
+            Array.Sort(arr);
+
+            const int expectedIndex = 3;
+            var x = arr[expectedIndex];
+            var actualIndex = searcher.FindIndex(arr, x);
+
+            // Assert
+            Assert.AreEqual(expectedIndex, actualIndex);
+        }
+
+       
+
+
+        [Test]
+        public static void FindIndex_ItemMissing_ItemNotFoundExceptionThrown()
+        {
+            // Arrange
+            var searcher = new FastSearcher();
+            var arr = new int[10];
+            for (var i = 0; i < arr.Length; i++)
+            {
+                arr[i] = i;
+            }
+
+            Array.Sort(arr);
+            // Act
+            const int x = 12;
+            // Assert
+            _ = Assert.Throws(typeof(ItemNotFoundException), () => searcher.FindIndex(arr, x));
+        }
+    }
+}
