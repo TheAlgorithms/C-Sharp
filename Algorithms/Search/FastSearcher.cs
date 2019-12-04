9e92ab5b34a6c4ac13a0117b029a9445b3374321
Witteborn <Witteborn@posteo.de>
2019-12-04 16:39:58 +0100

    Pass array as a parameter, not as a field in FastSearch (thread-safe)

--- Algorithms/Search/FastSearcher.cs
+++ Algorithms/Search/FastSearcher.cs
@@ -12,91 +12,70 @@ namespace Algorithms.Search
     /// </summary>
     public class FastSearcher
     {
-        private int[] array;
-
-        /// <summary>
-        /// Finds the index of the given item in the array.
-        /// </summary>
-        /// <param name="array">Defines the span of numbers.</param>
-        /// <param name="item">The number to be found.</param>
-        /// <returns>Returns the index of the item in the array.</returns>
-        public int FindIndex(int[] array, int item)
-        {
-            this.array = array;
-            int from = 0, to = array.Length - 1;
-            return FindIndex(from, to, item);
-        }
-
         /// <summary>
         /// Finds index of first item in array that satisfies specified term
         /// throws ItemNotFoundException if the item couldn't be found.
         /// </summary>
-        /// <param name="sectionStartIndex">Defines the lower boundary of the section.</param>
-        /// <param name="sectionEndIndex">Defines the upper boundary of the section.</param>
+        /// <param name="array">Span of sorted numbers which will be used to find the item.</param>
         /// <param name="item">Term to check against.</param>
         /// <returns>Index of first item that satisfies term.</returns>
-        private int FindIndex(int sectionStartIndex, int sectionEndIndex, int item)
+        /// <exception cref="ItemNotFoundException"> Gets thrown when the given item couldn't be found in the array.</exception>
+        public int FindIndex(int[] array, int item)
         {
-            if (sectionStartIndex < sectionEndIndex)
+            var indexBinary = array.Length / 2;
+
+            int[] section =
             {
-                var indexBinary = (sectionStartIndex + sectionEndIndex) / 2;
+                 array.Length - 1,
+                 item - array[0],
+                 array[array.Length - 1] - array[0],
+            };
 
-                int[] section =
-                {
-                 sectionEndIndex - sectionStartIndex,
-                 item - array[sectionStartIndex],
-                 array[sectionEndIndex] - array[sectionStartIndex],
-                };
-                if (section[2] == 0)
-                {
-                    section[2]++;
-                }
-
-                var indexInterpolation = sectionStartIndex + (section[0] * section[1] / section[2]);
-
-                indexInterpolation = Math.Abs(indexInterpolation);
-                indexBinary = Math.Abs(indexBinary);
-
-                if (indexInterpolation > array[sectionEndIndex])
-                {
-                    throw new ItemNotFoundException();
-                }
-
-                if (indexBinary > indexInterpolation)
-                {
-                    // Swap
-                    var temp = indexBinary;
-                    indexBinary = indexInterpolation;
-                    indexInterpolation = temp;
-                }
-
-                if (item == array[indexBinary])
-                {
-                    return indexBinary;
-                }
-                else if (item == array[indexInterpolation])
-                {
-                    return indexInterpolation;
-                }
-                else if (item < array[indexBinary])
-                {
-                    return FindIndex(sectionStartIndex, indexBinary - 1, item);
-                }
-                else if (item < array[indexInterpolation])
-                {
-                    return FindIndex(indexBinary + 1, indexInterpolation - 1, item);
-                }
-                else
-                {
-                    return FindIndex(indexInterpolation + 1, sectionEndIndex, item);
-                }
+            if (section[2] == 0)
+            {
+                section[2]++;
             }
-            else if (sectionStartIndex == sectionEndIndex)
+
+            var indexInterpolation = section[0] * section[1] / section[2];
+
+            indexInterpolation = Math.Abs(indexInterpolation);
+            indexBinary = Math.Abs(indexBinary);
+
+            if (indexInterpolation > array[array.Length - 1])
             {
-                if (array[sectionStartIndex] == item)
-                {
-                    return sectionStartIndex;
-                }
+                throw new ItemNotFoundException();
+            }
+
+            if (indexBinary > indexInterpolation)
+            {
+                // Swap
+                var temp = indexBinary;
+                indexBinary = indexInterpolation;
+                indexInterpolation = temp;
+            }
+
+            if (item == array[indexBinary])
+            {
+                return indexBinary;
+            }
+            else if (item == array[indexInterpolation])
+            {
+                return indexInterpolation;
+            }
+            else if (item < array[indexBinary])
+            {
+                var segment = new ArraySegment<int>(array, 0, indexBinary - 2).Array;
+                return FindIndex(segment, item);
+            }
+            else if (item < array[indexInterpolation])
+            {
+                var segment = new ArraySegment<int>(array, indexBinary + 1, indexInterpolation - 2).Array;
+                return FindIndex(segment, item);
+            }
+            else
+            {
+                var segment = new ArraySegment<int>(array, indexInterpolation + 1, array.Length - 1).Array;
+                return FindIndex(segment, item);
             }
 
             throw new ItemNotFoundException();
