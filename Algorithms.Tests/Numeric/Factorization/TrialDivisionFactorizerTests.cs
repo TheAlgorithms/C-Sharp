b0618a23a2599c7c27b2d1829f32cc5e693afc6b
Andrii Siriak <siryaka@gmail.com>
2019-12-26 10:25:57 +0200

    Add trial division factorization algorithm

---
+++ Algorithms.Tests/Numeric/Factorization/TrialDivisionFactorizerTests.cs
@@ -0,0 +1,25 @@
+﻿using Algorithms.Numeric.Factorization;
+using NUnit.Framework;
+
+namespace Algorithms.Tests.Numeric
+{
+    public static class TrialDivisionFactorizerTests
+    {
+        [Test]
+        [TestCase(2)]
+        [TestCase(3)]
+        [TestCase(29)]
+        [TestCase(31)]
+        public static void PrimeNumberFactorizationFails(int p)
+        {
+            // Arrange
+            var factorizer = new TrialDivisionFactorizer();
+
+            // Act
+            var success = factorizer.TryFactor(p, out _);
+
+            // Assert
+            Assert.IsFalse(success);
+        }
+    }
+}
