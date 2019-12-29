2344707e60cd59c2970960fc43cb15c246a342de
Andrii Siriak <siryaka@gmail.com>
2019-12-29 17:30:51 +0200

    Add Helper class

---
+++ Algorithms.Tests/Search/Helper.cs
@@ -0,0 +1,27 @@
+using System.Linq;
+using NUnit.Framework;
+
+namespace Algorithms.Tests.Search
+{
+    public static class Helper
+    {
+        public static int[] GetSortedArray(int length) => Enumerable.Range(0, length).Select(x => TestContext.CurrentContext.Random.Next(1_000_000)).OrderBy(x => x).ToArray();
+        
+        public static int GetItemIn(int[] arr) => arr[TestContext.CurrentContext.Random.Next(arr.Length)];
+
+        public static int GetItemNotIn(int[] arr)
+        {
+            int item;
+            do
+            {
+                item = TestContext.CurrentContext.Random.Next(arr.Min(), arr.Max() + 1);
+            } while (arr.Contains(item));
+
+            return item;
+        }
+
+        public static int GetItemSmallerThanAllIn(int[] arr) => arr.Min() - 1;
+
+        public static int GetItemBiggerThanAllIn(int[] arr) => arr.Max() + 1;
+    }
+}
