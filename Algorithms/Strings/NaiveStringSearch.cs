ff5aae426bdae83d0f33cb48b518d529e45e47d6
Vasken Houdoverdov <vhcomptech@gmail.com>
2020-01-06 09:14:32 -0800

    Update NaiveStringSearch.cs

--- Algorithms/Strings/NaiveStringSearch.cs
+++ Algorithms/Strings/NaiveStringSearch.cs
@@ -3,44 +3,49 @@ using System.Collections.Generic;
 
 namespace Algorithms.Strings
 {
-    /// <summary>
-    /// Implements the traditional naive string matching algorithm in C#.
-    /// Cost:  O(n*m), where m is pattern length and n is content length.
-    /// </summary>
-    public static class NaiveStringSearch
-    {
+   /// <summary>
+   /// Implements the traditional naive string matching algorithm in C#.
+   /// </summary>
+   public static class NaiveStringSearch
+   {
+      /// <summary>
+      /// NaiveSearch(Content, Pattern) will return an array containing each index of Content in which Pattern appears.
+      /// Cost:  O(n*m)
+      /// </summary>
       public static int[] NaiveSearch(String content, String pattern)
       {
-          int m = pattern.Length;
-          int n = content.Length;
-          List<int> indices = new List<int>();
-
-          for (int e = 0; e <= (n - m); e++)
-          {
+         int m = pattern.Length;
+         int n = content.Length;
+         List<int> indices = new List<int>();
+         
+         for (int e = 0; e <= (n - m); e++)
+         {
             int j;
-
+            
             for (j = 0; j < m; j++)
-                {
-                  if (content[e + j] != pattern[j])
-                  {
+            {
+               if (content[e + j] != pattern[j])
+               {
                   break;
-                  }
-                }
-
+               }
+            }
+            
             if (j == m)
-                {
-                indices.Add(e);
-                }
-          }
-
-           return indices.ToArray();
+            {
+               indices.Add(e);
+            }
+         }
+         
+         return indices.ToArray();
       }
-
+      /// <summary>
+      /// Main() demonstrates the Naive string search against a sample content body and pattern.
+      /// </summary>
       public static void Main()
       {
-        String content = "The foxy cheetah hunted a brown fox";
-        String pattern = "fox";
-        NaiveSearch(content, pattern);
+         String content = "The foxy cheetah hunted a brown fox";
+         String pattern = "fox";
+         NaiveSearch(content, pattern);
       }
-    }
+   }
 }
