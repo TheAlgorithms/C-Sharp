d5e9bb187def2d903220e4f359ff82419060f74b
Leon Budnick <leon.home@arcor.de>
2020-02-18 15:56:38 +0100

    array and list based stack

---
+++ DataStructures.Tests/ListBasedStackTests.cs
@@ -0,0 +1,71 @@
+﻿using DataStructures.ListBasedStack;
+
+using NUnit.Framework;
+
+namespace DataStructures.Tests
+{
+    public static class ListBasedStackTests
+    {
+        [Test]
+        public static void CountTest()
+        {
+            var stack = new ListBasedStack<int>(new[] { 0, 1, 2, 3, 4 });
+            Assert.IsTrue(stack.Count == 5);
+        }
+
+        [Test]
+        public static void ClearTest()
+        {
+            var stack = new ListBasedStack<int>(new[] { 0, 1, 2, 3, 4 });
+            stack.Clear();
+            Assert.IsTrue(stack.Count == 0);
+        }
+
+        [Test]
+        public static void ContainsTest()
+        {
+            var stack = new ListBasedStack<int>(new[] { 0, 1, 2, 3, 4 });
+            Assert.IsTrue(stack.Contains(0));
+            Assert.IsTrue(stack.Contains(1));
+            Assert.IsTrue(stack.Contains(2));
+            Assert.IsTrue(stack.Contains(3));
+            Assert.IsTrue(stack.Contains(4));
+        }
+
+        [Test]
+        public static void PeekTest()
+        {
+            var stack = new ListBasedStack<int>(new[] { 0, 1, 2, 3, 4 });
+            Assert.IsTrue(stack.Peek() == 4);
+            Assert.IsTrue(stack.Peek() == 4);
+            Assert.IsTrue(stack.Peek() == 4);
+        }
+
+        [Test]
+        public static void PopTest()
+        {
+            var stack = new ListBasedStack<int>(new[] { 0, 1, 2, 3, 4 });
+            Assert.IsTrue(stack.Pop() == 4);
+            Assert.IsTrue(stack.Pop() == 3);
+            Assert.IsTrue(stack.Pop() == 2);
+            Assert.IsTrue(stack.Pop() == 1);
+            Assert.IsTrue(stack.Pop() == 0);
+        }
+
+        [Test]
+        public static void PushTest()
+        {
+            var stack = new ListBasedStack<int>();
+            stack.Push(0);
+            Assert.IsTrue(stack.Peek() == 0);
+            stack.Push(1);
+            Assert.IsTrue(stack.Peek() == 1);
+            stack.Push(2);
+            Assert.IsTrue(stack.Peek() == 2);
+            stack.Push(3);
+            Assert.IsTrue(stack.Peek() == 3);
+            stack.Push(4);
+            Assert.IsTrue(stack.Peek() == 4);
+        }
+    }
+}
---
+++ DataStructures/ListBasedStack/ListBasedStack.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+
+namespace DataStructures.ListBasedStack
+{
+    /// <summary>
+    /// Implementation of a list based stack. FIFO style.
+    /// </summary>
+    /// <typeparam name="T">Generic Type.</typeparam>
+    public class ListBasedStack<T>
+    {
+        /// <summary>
+        /// <see cref="List{T}"/> based stack.
+        /// </summary>
+        private readonly List<T> stack;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ListBasedStack{T}"/> class.
+        /// </summary>
+        public ListBasedStack() => stack = new List<T>();
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ListBasedStack{T}"/> class.
+        /// </summary>
+        /// <param name="item">Item to push onto the <see cref="ListBasedStack{T}"/>.</param>
+        public ListBasedStack(T item)
+        : this() => Push(item);
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ListBasedStack{T}"/> class.
+        /// </summary>
+        /// <param name="items">Items to push onto the <see cref="ListBasedStack{T}"/>.</param>
+        public ListBasedStack(IEnumerable<T> items)
+        : this()
+        {
+            foreach (var item in items)
+            {
+                Push(item);
+            }
+        }
+
+        /// <summary>
+        /// Gets the number of elements on the <see cref="ListBasedStack{T}"/>.
+        /// </summary>
+        public int Count => stack.Count;
+
+        /// <summary>
+        /// Removes all items from the <see cref="ListBasedStack{T}"/>.
+        /// </summary>
+        public void Clear() => stack.Clear();
+
+        /// <summary>
+        /// Determines whether an element is in the <see cref="ListBasedStack{T}"/>.
+        /// </summary>
+        /// <param name="item">The item to locate in the <see cref="ListBasedStack{T}"/>.</param>
+        /// <returns>True, if the item is in the stack.</returns>
+        public bool Contains(T item) => stack.Contains(item);
+
+        /// <summary>
+        /// Returns the item at the top of the <see cref="ListBasedStack{T}"/> without removing it.
+        /// </summary>
+        /// <returns>The item at the top of the <see cref="ListBasedStack{T}"/>.</returns>
+        public T Peek() => stack[^1];
+
+        /// <summary>
+        /// Removes and returns the item at the top of the <see cref="ListBasedStack{T}"/>.
+        /// </summary>
+        /// <returns>The item removed from the top of the <see cref="ListBasedStack{T}"/>.</returns>
+        public T Pop()
+        {
+            var item = stack[^1];
+            stack.RemoveAt(stack.Count - 1);
+            return item;
+        }
+
+        /// <summary>
+        /// Inserts an item at the top of the <see cref="ListBasedStack{T}"/>.
+        /// </summary>
+        /// <param name="item">The item to push onto the <see cref="ListBasedStack{T}"/>.</param>
+        public void Push(T item) => stack.Add(item);
+    }
+}
