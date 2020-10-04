4da1287438d6ef02772ca9652e2d1b304fc2e1ad
Walt <32001362+Walt280@users.noreply.github.com>
2020-10-04 05:35:49 -0400

    Add Binary Search Tree (#158)
    * Add binary search tree implementation.
    
    * Edit README.md to add an entry for Binary Search Tree.
    
    Co-authored-by: Andrii Siriak <siryaka@gmail.com>

---
+++ DataStructures/BinarySearchTree/BinarySearchTreeNode.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace DataStructures.BinarySearchTree
+{
+    /// <summary>
+    /// Generic node class for BinarySearchTree.
+    /// Keys for each node are immutable.
+    /// </summary>
+    /// <typeparam name="TKey">Type of key for the node. Keys must implement IComparable.</typeparam>
+    public class BinarySearchTreeNode<TKey> where TKey : IComparable<TKey>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BinarySearchTreeNode{TKey}"/> class.
+        /// </summary>
+        /// <param name="key">The key of this node.</param>
+        public BinarySearchTreeNode(TKey key) => Key = key;
+
+        /// <summary>
+        /// Gets the key for this node.
+        /// </summary>
+        public TKey Key { get; }
+
+        /// <summary>
+        /// Gets or sets the reference to a child node that precedes/comes before this node.
+        /// </summary>
+        public BinarySearchTreeNode<TKey>? Left { get; set; }
+
+        /// <summary>
+        /// Gets or sets the reference to a child node that follows/comes after this node.
+        /// </summary>
+        public BinarySearchTreeNode<TKey>? Right { get; set; }
+    }
+}
