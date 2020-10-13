79278c447800777e83b377858274fb2a19945203
Justin LeCheminant <jlechem@gmail.com>
2020-10-13 10:39:40 -0600

    Adding entry point class for sorted linked list

---
+++ DataStructures/SortedLinkedList/SortedLinkedList.cs
@@ -0,0 +1,18 @@
+﻿namespace DataStructures.SortedLinkedList
+{
+    /// <summary>
+    /// TODO.
+    /// </summary>
+    /// <typeparam name="T">TODO. 2.</typeparam>
+    public class SortedLinkedList<T>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SortedList{T}"/> class.
+        /// TODO.
+        /// </summary>
+        /// <param name="data">TODO. 2.</param>
+        public SortedLinkedList()
+        {
+        }
+    }
+}
---
+++ DataStructures/SortedLinkedList/SortedLinkedListNode.cs
@@ -0,0 +1,30 @@
+﻿namespace DataStructures.SortedLinkedList
+{
+    /// <summary>
+    /// TODO.
+    /// </summary>
+    /// <typeparam name="T">TODO. 2.</typeparam>
+    public class SortedLinkedListNode<T>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SortedLinkedListNode{T}"/> class.
+        /// TODO.
+        /// </summary>
+        /// <param name="data">TODO. 2.</param>
+        public SortedLinkedListNode(T data)
+        {
+            Data = data;
+            Next = null;
+        }
+
+        /// <summary>
+        /// Gets elements from current SortedLinkedList.
+        /// </summary>
+        public T Data { get; }
+
+        /// <summary>
+        /// Gets or sets tODO. TODO.
+        /// </summary>
+        public SortedLinkedListNode<T>? Next { get; set; }
+    }
+}
