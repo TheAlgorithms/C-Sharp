07f5917364e49c4f2908fc5d47e75f42726109f0
Nektarios <mrm.nektarios@gmail.com>
2020-10-17 20:30:16 +0300

    NM =>     Implemented Jump Search Algorithm     Tested it with 4 different tests

--- Algorithms.Tests/Search/JumpSearcherTests.cs
+++ Algorithms.Tests/Search/JumpSearcherTests.cs
@@ -1,10 +1,54 @@
-﻿using Algorithms.Search;
+﻿using System.Linq;
+
+using Algorithms.Searches;
+
 using NUnit.Framework;
+
 using Utilities.Exceptions;
 
 namespace Algorithms.Tests.Search
 {
+    [TestFixture]
     public static class JumpSearcherTests
     {
+        [Test]
+        public static void FindIndex_ItemPresent_IndexCorrect()
+        {
+            var searcher = new JumpSearcher();
+            var arr = Helper.GetSortedArray(1000);
+            var present = Helper.GetItemIn(arr);
+            var index = searcher.FindIndex(arr.ToList<int>(), present, 500);
+            Assert.AreEqual(present, arr[index]);
+        }
+
+        [Test]
+        public static void FindIndex_ItemPresent_IndexLessThanZero()
+        {
+            var searcher = new JumpSearcher();
+            var arr = Helper.GetSortedArray(1000);
+            var present = Helper.GetItemIn(arr);
+            var index = searcher.FindIndex(arr.ToList<int>(), present, -2);
+            Assert.AreEqual(present, arr[index]);
+        }
+
+        [Test]
+        public static void FindIndex_ItemPresent_IndexGreaterThanListLength()
+        {
+            var searcher = new JumpSearcher();
+            var arr = Helper.GetSortedArray(1000);
+            var present = Helper.GetItemIn(arr);
+            var index = searcher.FindIndex(arr.ToList<int>(), present, 1001);
+            Assert.AreEqual(present, arr[index]);
+        }
+
+        [Test]
+        public static void FindIndex_ItemNotFound()
+        {
+            var searcher = new JumpSearcher();
+            var arr = Helper.GetSortedArray(1000);
+            var present = arr[arr.Length - 1] + 1;
+            var index = searcher.FindIndex(arr.ToList<int>(), present, 100);
+            Assert.AreEqual(present, arr[index]);
+        }
     }
 }
--- Algorithms/Search/JumpSearcher.cs
+++ Algorithms/Search/JumpSearcher.cs
@@ -1,13 +1,64 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Algorithms.Searches
 {
-    /// <summary>
-    /// TODO.
-    /// </summary>
-    /// <typeparam name="T">TODO. 2.</typeparam>
-    public class JumpSearcher<T> where T : IComparable<T>
+    public class JumpSearcher
     {
+        public int FindIndex(List<int> sortedList, int toBeFound, int jumpStep = 0)
+        {
+            try
+            {
+                var prevInd = 0;
+                var currInd = 0;
+                var response = 0;
+
+                if (jumpStep < 0)
+                {
+                    throw new ArgumentException("01 Jump step is less than 0");
+                }
+                else if (jumpStep > sortedList.Count())
+                {
+                    throw new IndexOutOfRangeException("02 Jump step is greater than the length of the list");
+                }
+
+                jumpStep = (jumpStep == 0) ? (int)Math.Sqrt(sortedList.Count()) : jumpStep;
+
+                currInd += jumpStep;
+
+                while (sortedList[Math.Min(currInd, sortedList.Count()) - 1] < toBeFound)
+                {
+                    prevInd = currInd;
+                    currInd += jumpStep;
+
+                    if (prevInd > sortedList.Count())
+                    {
+                        throw new IndexOutOfRangeException("03 Did not found given number");
+                    }
+                }
+
+                for (var i = prevInd; i < currInd; i++)
+                {
+                    if (sortedList[i] == toBeFound)
+                    {
+                        response = i;
+                    }
+                }
+
+                return response;
+            }
+            catch (ArgumentException arExc)
+            {
+                Console.WriteLine(arExc.Message);
+                return -int.Parse(arExc.Message.Substring(0, 2));
+            }
+            catch (IndexOutOfRangeException outOfRangeExc)
+            {
+                Console.WriteLine(outOfRangeExc.Message);
+                return -int.Parse(outOfRangeExc.Message.Substring(0, 2));
+            }
+        }
     }
 }
