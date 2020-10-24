c8ec8abb80b6531b08b7cadbe8e654aad5a406e8
avenmia <mavend2@gmail.com>
2020-10-24 12:37:22 -0500

    Adding initial Fibonacci memoized code

---
+++ Algorithms/Problems/Fibonacci/FibonacciMemo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+
+namespace Algorithms.Problems.Fibonacci
+{
+    public class FibonacciMemo
+    {
+        /// <summary>
+        /// This is the memoized solution to the Fibonacci problem.
+        /// This solution runs in O(N) time.
+        /// </summary>
+        /// <param name="n">The nth Fibonacci number to find.</param>
+        /// <returns>The nth Fibonacci number.</returns>
+        public static BigInteger FibonacciNumberFinderMemo(int n)
+        {
+            if (n < 0)
+            {
+                throw new ArgumentException("N must be greater than or equal to 0");
+            }
+
+            List<BigInteger> memo = new List<BigInteger> { 0, 1 };
+
+            if (n < 2)
+            {
+                return memo[n];
+            }
+
+            for (int i = 2; i <= n; i++)
+            {
+                BigInteger nextFibNumber = memo[i - 1] + memo[i - 2];
+                memo.Add(nextFibNumber);
+            }
+
+            return memo.Last();
+        }
+    }
+}
