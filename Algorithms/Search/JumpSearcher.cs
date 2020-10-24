fad393ade964131dc2388ee4cf227f6db7c65a13
Shubham_Mondal <mondalshubham1@gmail.com>
2020-10-24 13:33:00 +0530

    build issues resolved.

--- Algorithms/Search/JumpSearcher.cs
+++ Algorithms/Search/JumpSearcher.cs
@@ -6,7 +6,7 @@ namespace Algorithms.Searches
     /// <summary>
     /// Class that implements jump search algorithm.
     /// </summary>
-    /// <typeparam name="T">Type of array element.</typeparam>
+    /// <typeparam name="T"> Type of array element. </typeparam>
     public class JumpSearcher<T> where T : IComparable<T>
     {
         /// <summary>
@@ -15,33 +15,39 @@ namespace Algorithms.Searches
         /// space complexity: O(1),
         /// where n - array size.
         /// </summary>
-        /// <param name="sortedData">Sorted array to search in.</param>
-        /// <param name="item">Item to search for.</param>
-        /// <returns>Index of item that equals to item searched for or -1 if none found.</returns>
+        /// <param name="sortedData"> Sorted array to search in. </param>
+        /// <param name="item"> Item to search for. </param>
+        /// <returns> Index of item that equals to item searched for or -1 if none found. </returns>
         public int FindIndex(T[] sortedData, T item)
         {
             int n = sortedData.Length;
-            int step = Math.Sqrt(n);
+            int step = Convert.ToInt32(Math.Sqrt(n));
             int prev = 0;
 
-            while (item.CompareTo(sortedData[Math.Min(step, n)-1]) > 0) 
-            { 
-                prev = step; 
-                step += Math.Sqrt(n); 
-                if (prev >= n) 
-                    return -1; 
-            } 
+            while (item.CompareTo(sortedData[Math.Min(step, n) - 1]) > 0)
+            {
+                prev = step;
+                step += Convert.ToInt32(Math.Sqrt(n));
+                if (prev >= n)
+                {
+                    return -1;
+                }
+            }
 
-            while (item.CompareTo(sortedData[prev]) > 0) 
-            { 
-                prev++; 
-                if (prev == Math.Min(step, n)) 
-                    return -1; 
-            } 
+            while (item.CompareTo(sortedData[prev]) > 0)
+            {
+                prev++;
+                if (prev == Math.Min(step, n))
+                {
+                    return -1;
+                }
+            }
 
-            if (item.CompareTo(sortedData[prev]) == 0) 
-                return prev; 
-            
+            if (item.CompareTo(sortedData[prev]) == 0)
+            {
+                return prev;
+            }
+
             return -1;
         }
     }
