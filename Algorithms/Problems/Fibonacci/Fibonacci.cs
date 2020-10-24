2470c38c485ca07b25dfa8de97e29765778e6dfd
avenmia <mavend2@gmail.com>
2020-10-24 12:35:24 -0500

    Adding initial Fibonacci Algorithm

---
+++ Algorithms/Problems/Fibonacci/Fibonacci.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace Algorithms.Problems.Fibonacci
+{
+    public static class Fibonacci
+    {
+        /// <summary>
+        /// The Fibonacci sequence is defined as a N = (N -1) + (N - 2).
+        /// Given a number n, this function returns that corresponding
+        /// Fibonacci number in 2 ^ n time.
+        ///
+        /// For example, if n = 5 this function will return the 4th
+        /// Fibonacci number which woud be 3 (0, 1, 1, 2, 3).
+        ///
+        /// </summary>
+        /// <param name="n">The nth Fibonacci number to find.</param>
+        /// <returns>The nth Fibonacci number.</returns>
+        public static BigInteger FibonacciNumberFinder(int n)
+        {
+            if (n <= 1)
+            {
+                return n;
+            }
+
+            return FibonacciNumberFinder(n - 1) + FibonacciNumberFinder(n - 2);
+        }
+    }
+}
