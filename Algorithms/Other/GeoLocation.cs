231e002604a36e84e2195a48def8c9ac02b1db7e
Phuwarin Janaporn <phuwarin.21@gmail.com>
2020-10-25 19:10:15 +0700

    Fix naming variable and code convension.

--- Algorithms/Other/GeoLocation.cs
+++ Algorithms/Other/GeoLocation.cs
@@ -6,38 +6,31 @@ namespace Algorithms.Other
 {
     public static class GeoLocation
     {
-        private const double EarthRadius = 6371.01d;
+        private const double EarthRadiusKm = 6371.01d;
 
         /// <summary>
-        /// Calculate distance from 2 point of latitude, longitude from https://www.movable-type.co.uk/scripts/latlong.html.
+        /// Calculates spherical distance between 2 points given their latitude, longitude coordinates. https://www.movable-type.co.uk/scripts/latlong.html.
         /// </summary>
         /// <param name="lat1">Latitude of point A.</param>
         /// <param name="lng1">Longitude of point A.</param>
         /// <param name="lat2">Latitude of point B.</param>
         /// <param name="lng2">Longitude of point B.</param>
-        /// <returns>Distance between point A to point B.</returns>
-        public static decimal CalculateDistanceFromLatLng(double lat1, double lng1, double lat2, double lng2)
+        /// <returns>Spherical distance between A and B.</returns>
+        public static double CalculateDistanceFromLatLng(double lat1, double lng1, double lat2, double lng2)
         {
-            try
-            {
-                var pi180 = Math.PI / 180d;
-                var newLat1 = lat1 * pi180;
-                var newLng1 = lng1 * pi180;
-                var newLat2 = lat2 * pi180;
-                var newLng2 = lng2 * pi180;
+            var pi180 = Math.PI / 180d;
+            var lat1Radian = lat1 * pi180;
+            var lng1Radian = lng1 * pi180;
+            var lat2Radian = lat2 * pi180;
+            var lng2Radian = lng2 * pi180;
 
-                var diffLat = newLat2 - newLat1;
-                var diffLng = newLng2 - newLng1;
+            var diffLat = lat2Radian - lat1Radian;
+            var diffLng = lng2Radian - lng1Radian;
 
-                var haversine = Math.Sin(diffLat / 2) * Math.Sin(diffLat / 2) + Math.Cos(newLat1) * Math.Cos(newLat2) * Math.Sin(diffLng / 2) * Math.Sin(diffLng / 2);
-                var distance = EarthRadius * (2d * Math.Atan2(Math.Sqrt(haversine), Math.Sqrt(1 - haversine)));
+            var haversine = Math.Sin(diffLat / 2) * Math.Sin(diffLat / 2) + Math.Cos(lat1Radian) * Math.Cos(lat2Radian) * Math.Sin(diffLng / 2) * Math.Sin(diffLng / 2);
+            var distance = EarthRadiusKm * (2d * Math.Atan2(Math.Sqrt(haversine), Math.Sqrt(1 - haversine)));
 
-                return Convert.ToDecimal(distance * 1000); // Convert from km -> m
-            }
-            catch (Exception)
-            {
-                return 0m;
-            }
+            return distance * 1000; // Convert from km -> m
         }
     }
 }
