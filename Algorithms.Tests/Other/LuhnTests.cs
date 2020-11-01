382b5c9ee25e9411edf361e610b16fb3c48cd564
Nikita <myna7047@gmail.com>
2020-11-01 02:11:06 +0200

    Validation method tests

---
+++ Algorithms.Tests/Other/LuhnTests.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using Algorithms.Other;
+
+namespace Algorithms.Tests.Other
+{
+    /// <summary>
+    /// A class for testing the Luhn algorithm.
+    /// </summary>
+    public class LuhnTests
+    {
+        [Test]
+        [TestCase("89014103211118510720")] // ICCID
+        [TestCase("071052120")] // Social Security Code
+        [TestCase("449125546588769")] // IMEI
+        [TestCase("4417123456789113")] // Bank card
+        public void ValidateTrue(string number)
+        {
+            // Arrange
+            bool validate;
+
+            // Act
+            validate = Luhn.Validate(number);
+
+            // Assert
+            Assert.True(validate);
+        }
+
+        [Test]
+        [TestCase("89012104211118510720")] // ICCID
+        [TestCase("021053120")] // Social Security Code
+        [TestCase("449145545588969")] // IMEI
+        [TestCase("4437113456749113")] // Bank card
+        public void ValidateFalse(string number)
+        {
+            // Arrange
+            bool validate;
+
+            // Act
+            validate = Luhn.Validate(number);
+
+            // Assert
+            Assert.False(validate);
+        }
+    }
+}
