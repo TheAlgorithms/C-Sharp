cf25ea37c0942b6df09484a1f6667c94afc81224
Nikita <myna7047@gmail.com>
2020-11-01 02:24:13 +0200

    Tests for the number search method

--- Algorithms.Tests/Other/LuhnTests.cs
+++ Algorithms.Tests/Other/LuhnTests.cs
@@ -41,5 +41,24 @@ namespace Algorithms.Tests.Other
             // Assert
             Assert.False(validate);
         }
+
+        [Test]
+        [TestCase("x9012104211118510720")] // ICCID
+        [TestCase("0210x3120")] // Social Security Code
+        [TestCase("44914554558896x")] // IMEI
+        [TestCase("4437113456x49113")] // Bank card
+        public void GetLostNum(string number)
+        {
+            // Arrange
+            int lostNum;
+            bool validate;
+
+            // Act
+            lostNum = Luhn.GetLostNum(number);
+            validate = Luhn.Validate(number.Replace("x", lostNum.ToString()));
+
+            // Assert
+            Assert.True(validate);
+        }
     }
 }
