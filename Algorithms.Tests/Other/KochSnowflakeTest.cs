f13036c505aad1b59af337a3ac6705408bc3045b
algobytewise <algobytewise@gmail.com>
2021-03-30 15:32:25 +0530

    update KochSnowflakeTest.cs

--- Algorithms.Tests/Other/KochSnowflakeTest.cs
+++ Algorithms.Tests/Other/KochSnowflakeTest.cs
@@ -12,30 +12,30 @@ namespace Algorithms.Tests.Other
         [Test]
         public static void TestIterateMethod()
         {
-            List<Vector2> vectors = new List<Vector2>{new Vector2(0, 0),new Vector2(1, 0)};
+            List<Vector2> vectors = new List<Vector2> {new Vector2(0, 0), new Vector2(1, 0)};
             List<Vector2> result = Algorithms.Other.KochSnowflake.Iterate(vectors, steps: 1);
             result[0].Should().Be(new Vector2(0, 0));
-            result[1].Should().Be(new Vector2((float)1/3, 0));
+            result[1].Should().Be(new Vector2((float) 1 / 3, 0));
             
             /* Should().BeApproximately() is not defined for Vector2 or float
             so the x-y-components have to be tested separately and the y-component needs to be cast to double*/
             result[2].X.Should().Be(0.5f);
-            ((double)result[2].Y).Should().BeApproximately(Math.Sin(Math.PI/3)/3, 0.0001);
+            ((double)result[2].Y).Should().BeApproximately(Math.Sin(Math.PI / 3) / 3, 0.0001);
             
-            result[3].Should().Be(new Vector2((float)2/3, 0));
+            result[3].Should().Be(new Vector2((float) 2 / 3, 0));
             result[4].Should().Be(new Vector2(1, 0));
         }
         
         [Test]
         public static void BitmapWidthIsZeroOrNegative_ThrowsArgumentOutOfRangeException()
         {
-            Assert.Throws<ArgumentOutOfRangeException>(() => Algorithms.Other.KochSnowflake.GetBitmap(null, bitmapWidth:-200));
+            Assert.Throws<ArgumentOutOfRangeException>(() => Algorithms.Other.KochSnowflake.GetBitmap(new List<Vector2> {}, bitmapWidth:-200));
         }
         
         [Test]
         public static void BitmapHeightIsZeroOrNegative_ThrowsArgumentOutOfRangeException()
         {
-            Assert.Throws<ArgumentOutOfRangeException>(() => Algorithms.Other.KochSnowflake.GetBitmap(null, bitmapHeight:0));
+            Assert.Throws<ArgumentOutOfRangeException>(() => Algorithms.Other.KochSnowflake.GetBitmap(new List<Vector2> {}, bitmapHeight:0));
         }
         
         [Test]
