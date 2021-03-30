35be32148bbf502c02a04105590fd95608ab1f44
algobytewise <algobytewise@gmail.com>
2021-03-30 15:09:26 +0530

    update KochSnowflake.cs

--- Algorithms/Other/KochSnowflake.cs
+++ Algorithms/Other/KochSnowflake.cs
@@ -1,7 +1,7 @@
 using System;
-using System.Numerics;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Numerics;
 
 namespace Algorithms.Other
 {
@@ -19,7 +19,7 @@ namespace Algorithms.Other
     /// (description adapted from https://en.wikipedia.org/wiki/Koch_snowflake )
     /// (for a more detailed explanation and an implementation in the
     /// Processing language, see  https://natureofcode.com/book/chapter-8-fractals/
-    /// #84-the-koch-curve-and-the-arraylist-technique )
+    /// #84-the-koch-curve-and-the-arraylist-technique ).
     /// </summary>
     public static class KochSnowflake
     {
@@ -35,60 +35,17 @@ namespace Algorithms.Other
         public static List<Vector2> Iterate(List<Vector2> initialVectors, int steps = 5)
         {
             List<Vector2> vectors = initialVectors;
-            for(int i = 0; i < steps; i++)
+            for (int i = 0; i < steps; i++)
             {
                 vectors = IterationStep(vectors);
             }
-            
+
             return vectors;
         }
 
         /// <summary>
-        /// Loops through each pair of adjacent vectors. Each line between two adjacent
-        /// vectors is divided into 4 segments by adding 3 additional vectors in-between
-        /// the original two vectors. The vector in the middle is constructed through a
-        /// 60 degree rotation so it is bent outwards.
-        /// </summary>
-        /// <param name="vectors">The vectors composing the shape to which
-        /// the algorithm is applied.</param>
-        /// <returns>The transformed vectors after the iteration-step.</returns>
-        private static List<Vector2> IterationStep(List<Vector2> vectors)
-        {
-            List<Vector2> newVectors = new List<Vector2>();
-            for(int i = 0; i < vectors.Count - 1; i++)
-            {
-                Vector2 startVector = vectors[i];
-                Vector2 endVector = vectors[i + 1];
-                newVectors.Add(startVector);
-                Vector2 differenceVector = endVector - startVector;
-                newVectors.Add(startVector + differenceVector / 3);
-                newVectors.Add(
-                    startVector + differenceVector / 3 + Rotate(differenceVector / 3, 60)
-                );
-                newVectors.Add(startVector + differenceVector * 2 / 3);
-            }
-            newVectors.Add(vectors[vectors.Count-1]);
-            return newVectors;
-        }
-
-        /// <summary>
-        /// Standard rotation of a 2D vector with a rotation matrix
-        /// (see https://en.wikipedia.org/wiki/Rotation_matrix )
-        /// <summary>
-        /// <param name="vector">The vector to be rotated.</param>
-        /// <param name="angleInDegrees">The angle by which to rotate the vector.</param>
-        /// <returns>The rotated vector.</returns>
-        private static Vector2 Rotate(Vector2 vector, float angleInDegrees)
-        {
-            float radians = angleInDegrees * (float)Math.PI/180;
-            float ca = (float)Math.Cos(radians);
-            float sa = (float)Math.Sin(radians);
-            return new Vector2(ca*vector.X - sa*vector.Y, sa*vector.X + ca*vector.Y);
-        }
-
-        /// <summary>
         /// Utility-method to render the Koch snowflake to a bitmap.
-        /// <summary>
+        /// </summary>
         /// <param name="vectors">The vectors defining the edges to be rendered.</param>
         /// <param name="bitmapWidth">The width of the rendered bitmap.</param>
         /// <param name="bitmapHeight">The height of the rendered bitmap.</param>
@@ -110,16 +67,16 @@ namespace Algorithms.Other
             }
 
             Bitmap bitmap = new Bitmap(bitmapWidth, bitmapHeight);
-            
+
             // Set the background white
             using (Graphics graph = Graphics.FromImage(bitmap))
             {
-                Rectangle ImageSize = new Rectangle(0,0,bitmapWidth,bitmapHeight);
+                Rectangle ImageSize = new Rectangle(0, 0, bitmapWidth,bitmapHeight);
                 graph.FillRectangle(Brushes.White, ImageSize);
             }
-            
+
             // Draw the edges
-            for(int i = 0; i < vectors.Count-1; i++)
+            for (int i = 0; i < vectors.Count-1; i++)
             {
                 Pen blackPen = new Pen(Color.Black, 1);
 
@@ -128,7 +85,7 @@ namespace Algorithms.Other
                 float x2 = vectors[i+1].X;
                 float y2 = vectors[i+1].Y;
                 
-                using(var graphics = Graphics.FromImage(bitmap))
+                using (var graphics = Graphics.FromImage(bitmap))
                 {
                    graphics.DrawLine(blackPen, x1, y1, x2, y2);
                 }
@@ -140,16 +97,57 @@ namespace Algorithms.Other
         /// <summary>
         /// Example-method to render the Koch snowflake to a bitmap. To save the
         /// bitmap the command 'GetKochSnowflake().Save("KochSnowflake.png")' can be used.
-        /// <summary>
+        /// </summary>
         /// <returns>The bitmap of the rendered Koch snowflake.</returns>
         public static Bitmap GetKochSnowflake()
         {
             Vector2 vector1 = new Vector2(50, 150);
             Vector2 vector2 = new Vector2(300, (float)Math.Sin(Math.PI/3)*500 + 150);
             Vector2 vector3 = new Vector2(550, 150);
-            List<Vector2> initialVectors = new List<Vector2>(){vector1, vector2, vector3, vector1};
+            List<Vector2> initialVectors = new List<Vector2>{ vector1, vector2, vector3, vector1 };
             List<Vector2> vectors = Iterate(initialVectors);
             return GetBitmap(vectors);
+        }
+
+        /// <summary>
+        /// Loops through each pair of adjacent vectors. Each line between two adjacent
+        /// vectors is divided into 4 segments by adding 3 additional vectors in-between
+        /// the original two vectors. The vector in the middle is constructed through a
+        /// 60 degree rotation so it is bent outwards.
+        /// </summary>
+        /// <param name="vectors">The vectors composing the shape to which
+        /// the algorithm is applied.</param>
+        /// <returns>The transformed vectors after the iteration-step.</returns>
+        private static List<Vector2> IterationStep(List<Vector2> vectors)
+        {
+            List<Vector2> newVectors = new List<Vector2>();
+            for (int i = 0; i < vectors.Count - 1; i++)
+            {
+                Vector2 startVector = vectors[i];
+                Vector2 endVector = vectors[i + 1];
+                newVectors.Add(startVector);
+                Vector2 differenceVector = endVector - startVector;
+                newVectors.Add(startVector + differenceVector / 3);
+                newVectors.Add(startVector + differenceVector / 3 + Rotate(differenceVector / 3, 60));
+                newVectors.Add(startVector + differenceVector * 2 / 3);
+            }
+            newVectors.Add(vectors[vectors.Count-1]);
+            return newVectors;
+        }
+
+        /// <summary>
+        /// Standard rotation of a 2D vector with a rotation matrix
+        /// (see https://en.wikipedia.org/wiki/Rotation_matrix ).
+        /// </summary>
+        /// <param name="vector">The vector to be rotated.</param>
+        /// <param name="angleInDegrees">The angle by which to rotate the vector.</param>
+        /// <returns>The rotated vector.</returns>
+        private static Vector2 Rotate(Vector2 vector, float angleInDegrees)
+        {
+            float radians = angleInDegrees * (float)Math.PI/180;
+            float ca = (float)Math.Cos(radians);
+            float sa = (float)Math.Sin(radians);
+            return new Vector2(ca*vector.X - sa*vector.Y, sa*vector.X + ca*vector.Y);
         }
     }
 }
