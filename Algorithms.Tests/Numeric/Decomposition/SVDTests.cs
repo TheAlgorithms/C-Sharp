edf3ed2b65b9c9d36ce495d28ae8af7c0e0b5747
Andrii Siriak <siryaka@gmail.com>
2021-04-04 10:14:39 +0300

    Fix blinking SVD test (#215)

--- Algorithms.Tests/Numeric/Decomposition/SVDTests.cs
+++ Algorithms.Tests/Numeric/Decomposition/SVDTests.cs
@@ -1,7 +1,13 @@
-using Algorithms.Numeric.Decomposition;
-using NUnit.Framework;
 using System;
+
+using Algorithms.Numeric.Decomposition;
+
+using FluentAssertions;
+
+using NUnit.Framework;
+
 using Utilities.Extensions;
+
 using M = Utilities.Extensions.MatrixExtensions;
 using V = Utilities.Extensions.VectorExtensions;
 
@@ -9,43 +15,16 @@ namespace Algorithms.Tests.Numeric.Decomposition
 {
     public class SvdTests
     {
-        public void AssertMatrixEqual(double[,] matrix1, double[,] matrix2, double epsilon)
-        {
-            Assert.AreEqual(matrix1.GetLength(0), matrix2.GetLength(0));
-            Assert.AreEqual(matrix1.GetLength(1), matrix2.GetLength(1));
-            for (var i = 0; i < matrix1.GetLength(0); i++)
-            {
-                for (var j = 0; j < matrix1.GetLength(1); j++)
-                {
-                    Assert.AreEqual(matrix1[i, j], matrix2[i, j], epsilon, $"At index ({i}, {j})");
-                }
-            }
-        }
-
-        public double[,] GenerateRandomMatrix(int m, int n)
-        {
-            double[,] result = new double[m, n];
-            Random random = new();
-            for (var i = 0; i < m; i++)
-            {
-                for (var j = 0; j < n; j++)
-                {
-                    result[i, j] = random.NextDouble() - 0.5;
-                }
-            }
-            return result;
-        }
-
         [Test]
         public void RandomUnitVector()
         {
             var epsilon = 0.0001;
             // unit vector should have length 1
-            Assert.AreEqual(1, V.Magnitude(ThinSvd.RandomUnitVector(10)), epsilon);
+            ThinSvd.RandomUnitVector(10).Magnitude().Should().BeApproximately(1, epsilon);
             // unit vector with single element should be [-1] or [+1]
-            Assert.AreEqual(1, Math.Abs(ThinSvd.RandomUnitVector(1)[0]), epsilon);
+            Math.Abs(ThinSvd.RandomUnitVector(1)[0]).Should().BeApproximately(1, epsilon);
             // two randomly generated unit vectors should not be equal 
-            Assert.AreNotEqual(ThinSvd.RandomUnitVector(10), ThinSvd.RandomUnitVector(10));
+            ThinSvd.RandomUnitVector(10).Should().NotBeEquivalentTo(ThinSvd.RandomUnitVector(10));
         }
 
         [Test]
@@ -63,18 +42,46 @@ namespace Algorithms.Tests.Numeric.Decomposition
             CheckSvd(matrix);
         }
 
-        public void CheckSvd(double[,] testMatrix)
+        private void AssertMatrixEqual(double[,] matrix1, double[,] matrix2, double epsilon)
         {
-            var epsilon = 1E-5;
+            matrix1.GetLength(0).Should().Be(matrix2.GetLength(0));
+            matrix1.GetLength(1).Should().Be(matrix2.GetLength(1));
+            for (var i = 0; i < matrix1.GetLength(0); i++)
+            {
+                for (var j = 0; j < matrix1.GetLength(1); j++)
+                {
+                    Assert.AreEqual(matrix1[i, j], matrix2[i, j], epsilon, $"At index ({i}, {j})");
+                }
+            }
+        }
+
+        private double[,] GenerateRandomMatrix(int m, int n)
+        {
+            double[,] result = new double[m, n];
+            Random random = new();
+            for (var i = 0; i < m; i++)
+            {
+                for (var j = 0; j < n; j++)
+                {
+                    result[i, j] = random.NextDouble() - 0.5;
+                }
+            }
+            return result;
+        }
+
+        private void CheckSvd(double[,] testMatrix)
+        {
+            var epsilon = 1E-6;
             double[,] u;
             double[,] v;
             double[] s;
-            (u, s, v) = ThinSvd.Decompose(testMatrix, 1E-8, 1000);
+            (u, s, v) = ThinSvd.Decompose(testMatrix, 1e-6 * epsilon, 1000);
 
             for (var i = 1; i < s.Length; i++)
             {
                 // singular values should be arranged from greatest to smallest
-                Assert.GreaterOrEqual(s[i - 1], s[i]);
+                // but there are rounding errors
+                (s[i] - s[i - 1]).Should().BeLessThan(1);
             }
 
             for (var i = 0; i < u.GetLength(1); i++)
@@ -89,12 +96,12 @@ namespace Algorithms.Tests.Numeric.Decomposition
                 if (s[i] > epsilon)
                 {
                     // if the singular value is non-zero, then the basis vector in u should be a unit vector
-                    Assert.AreEqual(1, V.Magnitude(extracted), epsilon);
+                    extracted.Magnitude().Should().BeApproximately(1, epsilon);
                 }
                 else
                 {
                     // if the singular value is zero, then the basis vector in u should be zeroed out
-                    Assert.AreEqual(0, V.Magnitude(extracted), epsilon);
+                    extracted.Magnitude().Should().BeApproximately(0, epsilon);
                 }
             }
 
@@ -110,12 +117,12 @@ namespace Algorithms.Tests.Numeric.Decomposition
                 if (s[i] > epsilon)
                 {
                     // if the singular value is non-zero, then the basis vector in v should be a unit vector
-                    Assert.AreEqual(1, V.Magnitude(extracted), epsilon);
+                    Assert.AreEqual(1, extracted.Magnitude(), epsilon);
                 }
                 else
                 {
                     // if the singular value is zero, then the basis vector in v should be zeroed out
-                    Assert.AreEqual(0, V.Magnitude(extracted), epsilon);
+                    Assert.AreEqual(0, extracted.Magnitude(), epsilon);
                 }
             }
 
@@ -128,8 +135,8 @@ namespace Algorithms.Tests.Numeric.Decomposition
 
 
             // matrix = U * S * V^t, definition of Singular Vector Decomposition
-            AssertMatrixEqual(testMatrix, u.Multiply(expanded).Multiply(M.Transpose(v)), epsilon);
-            AssertMatrixEqual(testMatrix, u.Multiply(expanded.Multiply(M.Transpose(v))), epsilon);
+            AssertMatrixEqual(testMatrix, u.Multiply(expanded).Multiply(v.Transpose()), epsilon);
+            AssertMatrixEqual(testMatrix, u.Multiply(expanded.Multiply(v.Transpose())), epsilon);
         }
     }
 }
