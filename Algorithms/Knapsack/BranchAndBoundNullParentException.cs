11b905e9feb6b1d34ad64dc5c8707bea214829d7
threelittle87 <threelittle87@gmail.com>
2021-07-29 14:12:22 +0800

    Fix codacy issue

--- Algorithms/Knapsack/BranchAndBoundNullParentException.cs
+++ Algorithms/Knapsack/BranchAndBoundNullParentException.cs
@@ -1,12 +1,28 @@
 using System;
+using System.Runtime.Serialization;
 
 namespace Algorithms.Knapsack
 {
     [Serializable]
     public class BranchAndBoundNullParentException : Exception
     {
+        public BranchAndBoundNullParentException()
+        {
+        }
+
         public BranchAndBoundNullParentException(string msg)
         : base(msg)
+        {
+        }
+
+        public BranchAndBoundNullParentException(string message, Exception innerException)
+        : base(message, innerException)
+        {
+        }
+
+        // Without this constructor, deserialization will fail
+        protected BranchAndBoundNullParentException(SerializationInfo info, StreamingContext context)
+        : base(info, context)
         {
         }
     }
