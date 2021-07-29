312f5e253f141769acb95789ef7491b1a85c444d
threelittle87 <threelittle87@gmail.com>
2021-07-29 13:46:34 +0800

    Fix codacy issue.

--- Algorithms/Knapsack/BranchAndBoundNullParentException.cs
+++ Algorithms/Knapsack/BranchAndBoundNullParentException.cs
@@ -1,6 +1,9 @@
+using System;
+using System.Runtime.Serialization;
+
 namespace Algorithms.Knapsack
 {
-    public class BranchAndBoundNullParentException : System.Exception
+    public class BranchAndBoundNullParentException : Exception, ISerializable
     {
         public BranchAndBoundNullParentException(string msg)
         : base(msg)
