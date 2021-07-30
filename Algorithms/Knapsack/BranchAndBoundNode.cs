ac8df7f717959f47a94f2813b865f6529ab51ef2
threelittle87 <threelittle87@gmail.com>
2021-07-30 15:36:31 +0800

    Update BranchAndBoundNode.cs

--- Algorithms/Knapsack/BranchAndBoundNode.cs
+++ Algorithms/Knapsack/BranchAndBoundNode.cs
@@ -8,19 +8,19 @@ namespace Algorithms.Knapsack
         private readonly bool taken;
 
         // cumulativeWeight --> um of weight of item associated in each nodes starting from root to this node (only item that is taken)
-        private int cumulativeWeight;
+        public int CumulativeWeight { get; set; }
 
         // cumulativeValue --> sum of value of item associated in each nodes starting from root to this node (only item that is taken)
-        private double cumulativeValue;
+        public double CumulativeValue { get; set; }
 
         // upperBound --> largest possible value after taking/not taking the item associated to this node (fractional)
-        private double upperBound;
+        public double UpperBound { get; set; }
 
         // level --> level of the node in the tree structure
-        private int level;
+        public int Level { get; set; }
 
         // parent node
-        private BranchAndBoundNode? parent;
+        public BranchAndBoundNode? Parent { get; set; }
 
         public BranchAndBoundNode()
         {
@@ -28,71 +28,14 @@ namespace Algorithms.Knapsack
 
         public BranchAndBoundNode(int level, bool taken, BranchAndBoundNode parent)
         {
-            this.level = level;
+            this.Level = level;
             this.taken = taken;
-            this.parent = parent;
-        }
-
-        public int GetCumulativeWeight()
-        {
-            return cumulativeWeight;
-        }
-
-        public void SetCumulativeWeight(int cumulativeWeight)
-        {
-            this.cumulativeWeight = cumulativeWeight;
-        }
-
-        public double GetCumulativeValue()
-        {
-            return cumulativeValue;
-        }
-
-        public void SetCumulativeValue(double cumulativeValue)
-        {
-            this.cumulativeValue = cumulativeValue;
-        }
-
-        public double GetUpperBound()
-        {
-            return upperBound;
-        }
-
-        public void SetUpperBound(double upperBound)
-        {
-            this.upperBound = upperBound;
-        }
-
-        public int GetLevel()
-        {
-            return level;
-        }
-
-        public void SetLevel(int level)
-        {
-            this.level = level;
+            this.Parent = parent;
         }
 
         public bool IsTaken()
         {
             return taken;
-        }
-
-        public BranchAndBoundNode? GetParent()
-        {
-            if (parent is null)
-            {
-                throw new BranchAndBoundNullParentException();
-            }
-            else
-            {
-                return parent;
-            }
-        }
-
-        public void SetParent(BranchAndBoundNode parent)
-        {
-            this.parent = parent;
         }
     }
 }
