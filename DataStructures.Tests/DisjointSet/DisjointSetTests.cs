48e9ecb006b6e2635bb7264628e2bb7f23ddaeeb
Nimrod Rak <nimrod.rak@mail.huji.ac.il>
2021-10-01 22:44:33 +0300

    Added Disjoint Set implementation and unit tests

---
+++ DataStructures.Tests/DisjointSet/DisjointSetTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+
+namespace DataStructures.DisjointSet.Tests
+{
+    class DisjointSetTests
+    {
+        [Test]
+        public static void MakeTest()
+        {
+            DisjointSet<int> ds = new DisjointSet<int>();
+            var one = ds.MakeSet(1);
+            var two = ds.MakeSet(2);
+            Assert.IsTrue(one.Data == 1);
+            Assert.IsTrue(two.Data == 2);
+        }
+        [Test]
+        public static void UnionTest()
+        {
+            DisjointSet<int> ds = new DisjointSet<int>();
+            var one = ds.MakeSet(1);
+            var two = ds.MakeSet(2);
+            var three = ds.MakeSet(3);
+            ds.UnionSet(one, two);
+            Assert.IsTrue(ds.FindSet(one) == ds.FindSet(two));
+            ds.UnionSet(one, three);
+            Assert.IsTrue(ds.FindSet(two) == ds.FindSet(three));
+            Assert.IsTrue(one.Rank + two.Rank + three.Rank == 1);
+        }
+    }
+}
---
+++ DataStructures/DisjointSet/Node.cs
@@ -0,0 +1,22 @@
+namespace DataStructures.DisjointSet
+{
+    /// <summary>
+    /// node class to be used by disjoint set to represent nodes in Disjoint Set forest.
+    /// </summary>
+    /// <typeparam name="T">generic type for data to be stored.</typeparam>
+    public class Node<T>
+    {
+        public int Rank { get; set; }
+
+        public Node<T> Parent { get; set; }
+
+        public T Data { get; set; }
+
+        public Node(T data)
+        {
+            Parent = this;
+            Rank = 0;
+            this.Data = data;
+        }
+    }
+}
