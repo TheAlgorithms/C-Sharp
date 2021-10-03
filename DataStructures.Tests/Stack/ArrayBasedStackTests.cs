4de8ee84ea6e285575511bb9157276a19c0fdebc
pBouillon <pro.pierre.bouillon@protonmail.com>
2021-10-03 11:20:45 +0200

    Improve bulk assertions in ArrayBasedStackTests

--- DataStructures.Tests/Stack/ArrayBasedStackTests.cs
+++ DataStructures.Tests/Stack/ArrayBasedStackTests.cs
@@ -1,4 +1,5 @@
-﻿using DataStructures.Stack;
+using DataStructures.Stack;
+
 using NUnit.Framework;
 
 namespace DataStructures.Tests.Stack
@@ -26,45 +27,62 @@ namespace DataStructures.Tests.Stack
         public static void ContainsTest()
         {
             var stack = new ArrayBasedStack<int>(new[] { 0, 1, 2, 3, 4 });
-            Assert.IsTrue(stack.Contains(0));
-            Assert.IsTrue(stack.Contains(1));
-            Assert.IsTrue(stack.Contains(2));
-            Assert.IsTrue(stack.Contains(3));
-            Assert.IsTrue(stack.Contains(4));
+            
+            Assert.Multiple(() =>
+            {
+                Assert.IsTrue(stack.Contains(0));
+                Assert.IsTrue(stack.Contains(1));
+                Assert.IsTrue(stack.Contains(2));
+                Assert.IsTrue(stack.Contains(3));
+                Assert.IsTrue(stack.Contains(4));
+            });
         }
 
         [Test]
         public static void PeekTest()
         {
             var stack = new ArrayBasedStack<int>(new[] { 0, 1, 2, 3, 4 });
-            Assert.IsTrue(stack.Peek() == 4);
-            Assert.IsTrue(stack.Peek() == 4);
-            Assert.IsTrue(stack.Peek() == 4);
+
+            Assert.Multiple(() =>
+            {
+                Assert.IsTrue(stack.Peek() == 4);
+                Assert.IsTrue(stack.Peek() == 4);
+                Assert.IsTrue(stack.Peek() == 4);
+            });
         }
 
         [Test]
         public static void PopTest()
         {
             var stack = new ArrayBasedStack<int>(new[] { 0, 1, 2, 3, 4 });
-            Assert.IsTrue(stack.Pop() == 4);
-            Assert.IsTrue(stack.Pop() == 3);
-            Assert.IsTrue(stack.Pop() == 2);
-            Assert.IsTrue(stack.Pop() == 1);
-            Assert.IsTrue(stack.Pop() == 0);
+
+            Assert.Multiple(() =>
+            {
+                Assert.IsTrue(stack.Pop() == 4);
+                Assert.IsTrue(stack.Pop() == 3);
+                Assert.IsTrue(stack.Pop() == 2);
+                Assert.IsTrue(stack.Pop() == 1);
+                Assert.IsTrue(stack.Pop() == 0);
+            });
         }
 
         [Test]
         public static void PushTest()
         {
             var stack = new ArrayBasedStack<int>();
+
             stack.Push(0);
             Assert.IsTrue(stack.Peek() == 0);
+
             stack.Push(1);
             Assert.IsTrue(stack.Peek() == 1);
+
             stack.Push(2);
             Assert.IsTrue(stack.Peek() == 2);
+
             stack.Push(3);
             Assert.IsTrue(stack.Peek() == 3);
+
             stack.Push(4);
             Assert.IsTrue(stack.Peek() == 4);
         }
@@ -72,13 +90,17 @@ namespace DataStructures.Tests.Stack
         [Test]
         public static void AutomaticResizesTest()
         {
-            var stack = new ArrayBasedStack<int>();
-            stack.Capacity = 2;
+            var stack = new ArrayBasedStack<int>
+            {
+                Capacity = 2,
+            };
+
             stack.Push(0);
             stack.Push(1);
             stack.Push(2);
             stack.Push(3);
             stack.Push(4);
+
             Assert.IsTrue(stack.Capacity > 2);
         }
 
