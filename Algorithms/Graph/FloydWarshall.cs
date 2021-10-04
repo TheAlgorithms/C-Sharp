83c7f2ad80e6bff63cbaadce8d61ee06b2146f83
Nimrod Rak <nimrod.rak@mail.huji.ac.il>
2021-10-04 20:57:37 +0300

    fixed complexity issues

--- Algorithms/Graph/FloydWarshall.cs
+++ Algorithms/Graph/FloydWarshall.cs
@@ -12,12 +12,37 @@ namespace Algorithms.Graph
         /// <summary>
         /// runs the algorithm.
         /// </summary>
-        /// <param name="graph"></param>
+        /// <param name="graph">graph upon which to run.</param>
         /// <returns>
         /// a 2D array of shortest paths between any two vertices.
         /// where there is no path between two vertices - double.PositiveInfinity is placed.
         /// </returns>
         public double[,] Run(DirectedWeightedGraph<T> graph)
+        {
+            var distances = SetupDistances(graph);
+            var vertexCount = distances.GetLength(0);
+            for (var k = 0; k < vertexCount; k++)
+            {
+                for (var i = 0; i < vertexCount; i++)
+                {
+                    for (var j = 0; j < vertexCount; j++)
+                    {
+                        distances[i, j] = distances[i, j] > distances[i, k] + distances[k, j]
+                        ? distances[i, k] + distances[k, j]
+                        : distances[i, j];
+                    }
+                }
+            }
+
+            return distances;
+        }
+
+        /// <summary>
+        /// setup adjacency matrix for use by main algorithm run.
+        /// </summary>
+        /// <param name="graph">graph to dissect adjacency matrix from.</param>
+        /// <returns>the adjacency matrix in the format mentioned in Run.</returns>
+        private double[,] SetupDistances(DirectedWeightedGraph<T> graph)
         {
             var distances = new double[graph.Count, graph.Count];
             for (int i = 0; i < distances.GetLength(0); i++)
@@ -25,8 +50,8 @@ namespace Algorithms.Graph
                 for (var j = 0; j < distances.GetLength(0); j++)
                 {
                     var dist = graph.AdjacentDistance(
-                        graph.Vertices[i] ?? throw new ArgumentNullException("Vertex."),
-                        graph.Vertices[j] ?? throw new ArgumentNullException("Vertex."));
+                        graph.Vertices[i] ?? throw new ArgumentNullException(null, "Vertex was null."),
+                        graph.Vertices[j] ?? throw new ArgumentNullException(null, "Vertex was null."));
                     distances[i, j] = dist != 0 ? dist : double.PositiveInfinity;
                 }
             }
@@ -34,20 +59,6 @@ namespace Algorithms.Graph
             for (var i = 0; i < distances.GetLength(0); i++)
             {
                 distances[i, i] = 0;
-            }
-
-            for (var k = 0; k < distances.GetLength(0); k++)
-            {
-                for (var i = 0; i < distances.GetLength(0); i++)
-                {
-                    for (var j = 0; j < distances.GetLength(0); j++)
-                    {
-                        if (distances[i, j] > distances[i, k] + distances[k, j])
-                        {
-                            distances[i, j] = distances[i, k] + distances[k, j];
-                        }
-                    }
-                }
             }
 
             return distances;
