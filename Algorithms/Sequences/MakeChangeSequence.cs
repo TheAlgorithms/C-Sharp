ba26e5b56dba1e44d082294aa624be96c880cf1c
Richard Vasquez <github.com@callahansplace.org>
2021-10-04 16:10:11 -0700

    Reduce index minimum for A000008

--- Algorithms/Sequences/MakeChangeSequence.cs
+++ Algorithms/Sequences/MakeChangeSequence.cs
@@ -21,11 +21,11 @@ namespace Algorithms.Sequences
         ///     </para>
         ///     <para>
         ///         Uses formula from OEIS page by Michael Somos
-        ///         along with first 18 values to prevent index issues.
+        ///         along with first 17 values to prevent index issues.
         ///     </para>
         ///     <para>
         ///         Formula:
-        ///         a(n) = a(n-2) +a(n-5) - a(n-7) + a(n-10) - a(n-12) - a(n-15) + a(n-17) + 1
+        ///         a(n) = a(n-2) +a(n-5) - a(n-7) + a(n-10) - a(n-12) - a(n-15) + a(n-17) + 1.
         ///     </para>
         /// </summary>
         public IEnumerable<BigInteger> Sequence
@@ -35,14 +35,14 @@ namespace Algorithms.Sequences
                 var seed = new List<BigInteger>
                            {
                                1, 1, 2, 2, 3, 4, 5, 6, 7, 8,
-                               11, 12, 15, 16, 19, 22, 25, 28,
+                               11, 12, 15, 16, 19, 22, 25,
                            };
                 foreach (var value in seed)
                 {
                     yield return value;
                 }
 
-                for(var index = 18; ; index++)
+                for(var index = 17; ; index++)
                 {
                     BigInteger newValue = seed[index - 2] + seed[index - 5] - seed[index - 7]
                                         + seed[index - 10] - seed[index - 12] - seed[index - 15]
