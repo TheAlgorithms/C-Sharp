c5cf1bf9535eb2b57cb91a3e8213a78a27230cbd
Leefrost <evertodante@gmail.com>
2021-10-08 22:59:13 +0300

    MinMax pairing heap straightforward implementation

---
+++ DataStructures.Tests/Heap/PairingHeap/PairingHeapTests.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Linq;
+using DataStructures.Heap.PairingHeap;
+using NUnit.Framework;
+
+namespace DataStructures.Tests.Heap.PairingHeap
+{
+    internal class PairingHeapTests
+    {
+        [Test]
+        public void BuildMinHeap_CreateHeap_HeapIsCheked()
+        {
+            var nodeCount = 1000 * 10;
+            var minHeap = new PairingHeap<int>();
+            for (var i = 0; i <= nodeCount; i++)
+            {
+                minHeap.Insert(i);
+            }
+
+            for (var i = 0; i <= nodeCount; i++)
+            {
+                minHeap.UpdateKey(i, i - 1);
+            }
+
+            var min = 0;
+            for (var i = 0; i <= nodeCount; i++)
+            {
+                min = minHeap.Extract();
+                Assert.AreEqual(min, i - 1);
+            }
+
+            Assert.AreEqual(minHeap.Count, minHeap.Count);
+
+            var rnd = new Random();
+            var testSeries = Enumerable.Range(0, nodeCount - 1).OrderBy(x => rnd.Next()).ToList();
+
+            foreach (var item in testSeries)
+            {
+                minHeap.Insert(item);
+            }
+
+            for (var i = 0; i < testSeries.Count; i++)
+            {
+                var decremented = testSeries[i] - rnd.Next(0, 1000);
+                minHeap.UpdateKey(testSeries[i], decremented);
+                testSeries[i] = decremented;
+            }
+
+            testSeries.Sort();
+
+            for (var i = 0; i < nodeCount - 2; i++)
+            {
+                min = minHeap.Extract();
+                Assert.AreEqual(testSeries[i], min);
+            }
+
+            Assert.AreEqual(minHeap.Count, minHeap.Count);
+        }
+
+        [Test]
+        public void BuildMaxHeap_CreateHeap_HeapIsCheked()
+        {
+            var nodeCount = 1000 * 10;
+            var maxHeap = new PairingHeap<int>(Sorting.Descending);
+            for (var i = 0; i <= nodeCount; i++)
+            {
+                maxHeap.Insert(i);
+            }
+
+            for (var i = 0; i <= nodeCount; i++)
+            {
+                maxHeap.UpdateKey(i, i + 1);
+            }
+
+            Assert.AreEqual(maxHeap.Count, maxHeap.Count);
+
+            var max = 0;
+            for (var i = nodeCount; i >= 0; i--)
+            {
+                max = maxHeap.Extract();
+                Assert.AreEqual(max, i + 1);
+            }
+
+            var rnd = new Random();
+            var testSeries = Enumerable.Range(0, nodeCount - 1).OrderBy(x => rnd.Next()).ToList();
+
+            foreach (var item in testSeries)
+            {
+                maxHeap.Insert(item);
+            }
+            for (var i = 0; i < testSeries.Count; i++)
+            {
+                var incremented = testSeries[i] + rnd.Next(0, 1000);
+                maxHeap.UpdateKey(testSeries[i], incremented);
+                testSeries[i] = incremented;
+            }
+
+            testSeries = testSeries.OrderByDescending(x => x).ToList();
+            for (var i = 0; i < nodeCount - 2; i++)
+            {
+                max = maxHeap.Extract();
+                Assert.AreEqual(testSeries[i], max);
+            }
+
+            Assert.AreEqual(maxHeap.Count, maxHeap.Count);
+        }
+    }
+}
---
+++ DataStructures/Heap/PairingHeap/PairingNodeComparer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataStructures.Heap.PairingHeap
+{
+    /// <summary>
+    /// Node comparer.
+    /// </summary>
+    /// <typeparam name="T">Node type.</typeparam>
+    internal class PairingNodeComparer<T> : IComparer<T> where T : IComparable
+    {
+        private readonly bool isMax;
+        private readonly IComparer<T> nodeComparer;
+
+        internal PairingNodeComparer(Sorting sortDirection, IComparer<T> comparer)
+        {
+            isMax = sortDirection == Sorting.Descending;
+            nodeComparer = comparer;
+        }
+
+        public int Compare(T? x, T? y)
+        {
+            return !isMax ? nodeComparer.Compare(x, y) : nodeComparer.Compare(y, x);
+        }
+    }
+}
