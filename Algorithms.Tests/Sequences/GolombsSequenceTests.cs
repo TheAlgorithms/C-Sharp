95103bf789497f1324f9354307fee962503a9c1b
Valdas <valdas771@gmail.com>
2021-10-10 19:46:52 +0300

    Add Golomb's sequence

---
+++ Algorithms.Tests/Sequences/GolombsSequenceTests.cs
@@ -0,0 +1,27 @@
+using System.Linq;
+using System.Numerics;
+using Algorithms.Sequences;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Algorithms.Tests.Sequences
+{
+    public class GolombsSequenceTests
+    {
+        [Test]
+        public void First50ElementsCorrect()
+        {
+            // Taken from https://oeis.org/A001462
+            var expected = new BigInteger[] {
+                1, 2, 2, 3, 3, 4, 4, 4, 5, 5,
+                5, 6, 6, 6, 6, 7, 7, 7, 7, 8,
+                8, 8, 8, 9, 9, 9, 9, 9, 10, 10,
+                10, 10, 10, 11, 11, 11, 11, 11, 12, 12,
+                12, 12, 12, 12, 13, 13, 13, 13, 13, 13};
+
+            var sequence = new GolombsSequence().Sequence.Take(50);
+
+            sequence.SequenceEqual(expected).Should().BeTrue();
+        }
+    }
+}
