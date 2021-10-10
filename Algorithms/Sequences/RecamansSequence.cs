c78a72b8433d04311ebf8f4bd76d9b291f5e6a7d
Valdas <valdemar.silcenko@gmail.com>
2021-10-10 07:49:48 +0300

    Add Recaman's sequence (#250)
    Co-authored-by: Valdas <valdas771@gmail.com>

---
+++ Algorithms/Sequences/RecamansSequence.cs
@@ -0,0 +1,47 @@
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace Algorithms.Sequences
+{
+    /// <summary>
+    ///     <para>
+    ///         Recaman's sequence. a(0) = 0; for n > 0, a(n) = a(n-1) - n if nonnegative and not already in the sequence, otherwise a(n) = a(n-1) + n.
+    ///     </para>
+    ///     <para>
+    ///         Wikipedia: https://en.wikipedia.org/wiki/Recam%C3%A1n%27s_sequence.
+    ///     </para>
+    ///     <para>
+    ///         OEIS: http://oeis.org/A005132.
+    ///     </para>
+    /// </summary>
+    public class RecamansSequence : ISequence
+    {
+        /// <summary>
+        ///     Gets Recaman's sequence.
+        /// </summary>
+        public IEnumerable<BigInteger> Sequence
+        {
+            get
+            {
+                yield return 0;
+                var elements = new HashSet<BigInteger> { 0 };
+                var previous = 0;
+                var i = 1;
+
+                while (true)
+                {
+                    var current = previous - i;
+                    if (current < 0 || elements.Contains(current))
+                    {
+                        current = previous + i;
+                    }
+
+                    yield return current;
+                    previous = current;
+                    elements.Add(current);
+                    i++;
+                }
+            }
+        }
+    }
+}
