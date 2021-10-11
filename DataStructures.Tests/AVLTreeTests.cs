7075eacc1d72e2a0725cd32e060eb3bf2ce57374
Mikel Woo <mwoo1@hawk.iit.edu>
2021-10-10 17:20:13 -0700

    Added Remove and Contains along with remaining needed tests.

--- DataStructures.Tests/AVLTreeTests.cs
+++ DataStructures.Tests/AVLTreeTests.cs
@@ -48,8 +48,156 @@ namespace DataStructures.Tests
             var tree = new AVLTree<char>();
             tree.AddRange(new[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g' });
             tree.Count.Should().Be(7);
-            tree.GetKeysInOrder().SequenceEqual(new[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g' });
-            tree.GetKeysPreOrder().SequenceEqual(new[] { 'd', 'b', 'a', 'c', 'f', 'e', 'g' });
+            tree.GetKeysInOrder().SequenceEqual(new[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g' }).Should().BeTrue();
+            tree.GetKeysPreOrder().SequenceEqual(new[] { 'd', 'b', 'a', 'c', 'f', 'e', 'g' }).Should().BeTrue();
+        }
+
+        [Test]
+        public void Remove_MultipleKeys_TreeStillValid()
+        {
+            var tree = new AVLTree<int>();
+            tree.AddRange(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
+
+            tree.Remove(7);
+            tree.Count.Should().Be(9);
+            tree.Contains(7).Should().BeFalse();
+            tree.GetKeysInOrder().SequenceEqual(new[] { 1, 2, 3, 4, 5, 6, 8, 9, 10 }).Should().BeTrue();
+            tree.GetKeysPreOrder().SequenceEqual(new[] { 4, 2, 1, 3, 8, 6, 5, 9, 10 }).Should().BeTrue();
+
+            tree.Remove(2);
+            tree.Count.Should().Be(8);
+            tree.Contains(2).Should().BeFalse();
+
+            tree.Remove(1);
+            tree.Count.Should().Be(7);
+            tree.Contains(1).Should().BeFalse();
+            tree.GetKeysInOrder().SequenceEqual(new[] { 3, 4, 5, 6, 8, 9, 10 }).Should().BeTrue();
+            tree.GetKeysPreOrder().SequenceEqual(new[] { 8, 4, 3, 6, 5, 9, 10 }).Should().BeTrue();
+
+            tree.Remove(9);
+            tree.Count.Should().Be(6);
+            tree.Contains(9).Should().BeFalse();
+            tree.GetKeysInOrder().SequenceEqual(new[] { 3, 4, 5, 6, 8, 10 }).Should().BeTrue();
+            tree.GetKeysPreOrder().SequenceEqual(new[] { 6, 4, 3, 5, 8, 10 }).Should().BeTrue();
+
+            tree.Remove(3);
+            tree.Remove(4);
+            tree.Remove(5);
+            tree.Remove(6);
+            tree.Remove(8);
+            tree.Remove(10);
+
+            tree.Count.Should().Be(0);
+            tree.GetKeysInOrder().SequenceEqual(Array.Empty<int>()).Should().BeTrue();
+        }
+
+        [Test]
+        public void Remove_EmptyTree_ThrowsException()
+        {
+            var tree = new AVLTree<int>();
+            Assert.Throws<InvalidOperationException>(() => tree.Remove(1));
+        }
+
+        [Test]
+        public void Remove_KeyNotInTree_ThrowsException()
+        {
+            var tree = new AVLTree<int>();
+            tree.AddRange(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
+            Assert.Throws<KeyNotFoundException>(() => tree.Remove(24));
+        }
+
+        [Test]
+        public void Contains_CorrectReturn()
+        {
+            var tree = new AVLTree<int>();
+            tree.AddRange(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
+            tree.Contains(3).Should().BeTrue();
+            tree.Contains(7).Should().BeTrue();
+            tree.Contains(24).Should().BeFalse();
+            tree.Contains(-1).Should().BeFalse();
+        }
+
+        [Test]
+        public void Contains_EmptyTree_ReturnsFalse()
+        {
+            var tree = new AVLTree<int>();
+            tree.Contains(5).Should().BeFalse();
+            tree.Contains(-12).Should().BeFalse();
+        }
+
+        [Test]
+        public void GetMin_CorrectReturn()
+        {
+            var tree = new AVLTree<int>();
+            tree.AddRange(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
+            tree.GetMin().Should().Be(1);
+        }
+
+        [Test]
+        public void GetMin_EmptyTree_ThrowsException()
+        {
+            var tree = new AVLTree<int>();
+            Assert.Throws<InvalidOperationException>(() => tree.GetMin());
+        }
+
+        [Test]
+        public void GetMax_CorrectReturn()
+        {
+            var tree = new AVLTree<int>();
+            tree.AddRange(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
+            tree.GetMax().Should().Be(10);
+        }
+
+        [Test]
+        public void GetMax_EmptyTree_ThrowsException()
+        {
+            var tree = new AVLTree<int>();
+            Assert.Throws<InvalidOperationException>(() => tree.GetMax());
+        }
+
+        [Test]
+        public void GetKeysInOrder_CorrectReturn()
+        {
+            var tree = new AVLTree<int>();
+            tree.AddRange(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
+            tree.GetKeysInOrder().SequenceEqual(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }).Should().BeTrue();
+        }
+
+        [Test]
+        public void GetKeysInOrder_EmptyTree_CorrectReturn()
+        {
+            var tree = new AVLTree<int>();
+            tree.GetKeysInOrder().SequenceEqual(Array.Empty<int>()).Should().BeTrue();
+        }
+
+        [Test]
+        public void GetKeysPreOrder_CorrectReturn()
+        {
+            var tree = new AVLTree<int>();
+            tree.AddRange(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
+            tree.GetKeysPreOrder().SequenceEqual(new[] { 4, 2, 1, 3, 8, 6, 5, 7, 9, 10 }).Should().BeTrue();
+        }
+
+        [Test]
+        public void GetKeysPreOrder_EmptyTree_CorrectReturn()
+        {
+            var tree = new AVLTree<int>();
+            tree.GetKeysPreOrder().SequenceEqual(Array.Empty<int>()).Should().BeTrue();
+        }
+
+        [Test]
+        public void GetKeysPostOrder_CorrectReturn()
+        {
+            var tree = new AVLTree<int>();
+            tree.AddRange(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
+            tree.GetKeysPostOrder().SequenceEqual(new[] { 1, 3, 2, 5, 7, 6, 10, 9, 8, 4 }).Should().BeTrue();
+        }
+
+        [Test]
+        public void GetKeysPostOrder_EmptyTree_CorrectReturn()
+        {
+            var tree = new AVLTree<int>();
+            tree.GetKeysPostOrder().SequenceEqual(Array.Empty<int>()).Should().BeTrue();
         }
     }
 }
--- DataStructures/AVLTree/AVLTree.cs
+++ DataStructures/AVLTree/AVLTree.cs
@@ -21,11 +21,24 @@ namespace DataStructures.AVLTree
     /// <typeparam name="TKey">Type of key for the tree.</typeparam>
     public class AVLTree<TKey>
     {
+        /// <summary>
+        ///     Gets the number of nodes in the tree.
+        /// </summary>
         public int Count { get; private set; }
 
+        /// <summary>
+        ///     Comparer to use when comparing key values.
+        /// </summary>
         private readonly Comparer<TKey> comparer;
+
+        /// <summary>
+        ///     Reference to the root node.
+        /// </summary>
         private AVLTreeNode<TKey>? root;
 
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="AVLTree{TKey}"/> class.
+        /// </summary>
         public AVLTree()
         {
             root = null;
@@ -33,6 +46,11 @@ namespace DataStructures.AVLTree
             Count = 0;
         }
 
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="AVLTree{TKey}"/> class
+        ///     using the specified comparer.
+        /// </summary>
+        /// <param name="customComparer">Comparer to use when comparing keys.</param>
         public AVLTree(Comparer<TKey> customComparer)
         {
             root = null;
@@ -40,6 +58,10 @@ namespace DataStructures.AVLTree
             Count = 0;
         }
 
+        /// <summary>
+        ///     Add a single node to the tree.
+        /// </summary>
+        /// <param name="key">Key value to add.</param>
         public void Add(TKey key)
         {
             if (root is null)
@@ -54,6 +76,10 @@ namespace DataStructures.AVLTree
             Count++;
         }
 
+        /// <summary>
+        ///     Add multiple nodes to the tree.
+        /// </summary>
+        /// <param name="keys">Key values to add.</param>
         public void AddRange(IEnumerable<TKey> keys)
         {
             foreach(var key in keys)
@@ -62,16 +88,30 @@ namespace DataStructures.AVLTree
             }
         }
 
-        public bool Remove(TKey key)
+        /// <summary>
+        ///     Remove a node from the tree.
+        /// </summary>
+        /// <param name="key">Key value to remove.</param>
+        public void Remove(TKey key)
         {
             if (root is null)
             {
-                return false;
+                throw new InvalidOperationException("Tree is empty!");
+            }
+            else if (!Contains(key))
+            {
+                throw new KeyNotFoundException($"Key {key} is not in the tree");
             }
 
-            return true;
+            root = Remove(root, key);
+            Count--;
         }
 
+        /// <summary>
+        ///     Check if given node is in the tree.
+        /// </summary>
+        /// <param name="key">Key value to search for.</param>
+        /// <returns>Whether or not the node is in the tree.</returns>
         public bool Contains(TKey key)
         {
             var node = root;
@@ -95,6 +135,10 @@ namespace DataStructures.AVLTree
             return false;
         }
 
+        /// <summary>
+        ///     Get the minimum value in the tree.
+        /// </summary>
+        /// <returns>Minimum value in tree.</returns>
         public TKey GetMin()
         {
             if(root is null)
@@ -105,6 +149,10 @@ namespace DataStructures.AVLTree
             return GetMin(root).Key;
         }
 
+        /// <summary>
+        ///     Get the maximum value in the tree.
+        /// </summary>
+        /// <returns>Maximum value in tree.</returns>
         public TKey GetMax()
         {
             if(root is null)
@@ -115,6 +163,10 @@ namespace DataStructures.AVLTree
             return GetMax(root).Key;
         }
 
+        /// <summary>
+        ///     Get keys in order from smallest to largest as defined by the comparer.
+        /// </summary>
+        /// <returns>Keys in tree in order from smallest to largest.</returns>
         public IEnumerable<TKey> GetKeysInOrder()
         {
             List<TKey> result = new List<TKey>();
@@ -134,6 +186,10 @@ namespace DataStructures.AVLTree
             }
         }
 
+        /// <summary>
+        ///     Get keys in the pre-order order.
+        /// </summary>
+        /// <returns>Keys in pre-order order.</returns>
         public IEnumerable<TKey> GetKeysPreOrder()
         {
             var result = new List<TKey>();
@@ -153,6 +209,10 @@ namespace DataStructures.AVLTree
             }
         }
 
+        /// <summary>
+        ///     Get keys in the post-order order.
+        /// </summary>
+        /// <returns>Keys in the post-order order.</returns>
         public IEnumerable<TKey> GetKeysPostOrder()
         {
             var result = new List<TKey>();
@@ -172,8 +232,15 @@ namespace DataStructures.AVLTree
             }
         }
 
+        /// <summary>
+        ///     Recursively function to add a node to the tree.
+        /// </summary>
+        /// <param name="node">Node to check for null leaf.</param>
+        /// <param name="key">Key value to add.</param>
+        /// <returns>New node with key inserted.</returns>
         private AVLTreeNode<TKey> Add(AVLTreeNode<TKey> node, TKey key)
         {
+            // Regular binary search tree insertion
             int compareResult = comparer.Compare(key, node.Key);
             if (compareResult < 0)
             {
@@ -204,41 +271,93 @@ namespace DataStructures.AVLTree
                 throw new ArgumentException($"Key \"{key}\" already exists in tree!");
             }
 
+            // Check all of the new node's ancestors for inbalance and perform
+            // necessary rotations
             node.UpdateBalanceFactor();
 
             return Rebalance(node);
         }
 
+        /// <summary>
+        ///     Recursive function to remove node from tree.
+        /// </summary>
+        /// <param name="node">Node to check for key.</param>
+        /// <param name="key">Key value to remove.</param>
+        /// <returns>New node with key removed.</returns>
+        private AVLTreeNode<TKey>? Remove(AVLTreeNode<TKey> node, TKey key)
+        {
+            // Normal binary search tree removal
+            var compareResult = comparer.Compare(key, node.Key);
+            if (compareResult < 0)
+            {
+                node.Left = Remove(node.Left!, key);
+            }
+            else if (compareResult > 0)
+            {
+                node.Right = Remove(node.Right!, key);
+            }
+            else
+            {
+                if (node.Left is null && node.Right is null)
+                {
+                    return null;
+                }
+                else if (node.Left is null)
+                {
+                    var successor = GetMin(node.Right!);
+                    node.Right = Remove(node.Right!, successor.Key);
+                    node.Key = successor.Key;
+                }
+                else
+                {
+                    var predecessor = GetMax(node.Left!);
+                    node.Left = Remove(node.Left!, predecessor.Key);
+                    node.Key = predecessor.Key;
+                }
+            }
+
+            // Check all of the removed node's ancestors for rebalance and
+            // perform necessary rotations.
+            node.UpdateBalanceFactor();
+
+            return Rebalance(node);
+        }
+
+        /// <summary>
+        ///     Helper function to rebalance the tree so that all nodes have a
+        ///     balance factor in the range [-1, 1].
+        /// </summary>
+        /// <param name="node">Node to rebalance.</param>
+        /// <returns>New node that has been rebalanced.</returns>
         private AVLTreeNode<TKey> Rebalance(AVLTreeNode<TKey> node)
         {
             if (node.BalanceFactor > 1)
             {
-                if (node.Right!.BalanceFactor == 1)
-                {
-                    node = RotateLeft(node);
-                }
-                else if (node.Right!.BalanceFactor == -1)
+                if (node.Right!.BalanceFactor == -1)
                 {
                     node.Right = RotateRight(node.Right);
-                    node = RotateLeft(node);
                 }
+
+                node = RotateLeft(node);
             }
             else if (node.BalanceFactor < -1)
             {
                 if (node.Left!.BalanceFactor == 1)
                 {
                     node.Left = RotateLeft(node.Left);
-                    node = RotateRight(node);
                 }
-                else if (node.Left!.BalanceFactor == -1)
-                {
-                    node = RotateRight(node);
-                }
+
+                node = RotateRight(node);
             }
 
             return node;
         }
 
+        /// <summary>
+        ///     Perform a left (counter-clockwise) rotation.
+        /// </summary>
+        /// <param name="node">Node to rotate about.</param>
+        /// <returns>New node with rotation applied.</returns>
         private AVLTreeNode<TKey> RotateLeft(AVLTreeNode<TKey> node)
         {
             var temp1 = node;
@@ -253,6 +372,11 @@ namespace DataStructures.AVLTree
             return node;
         }
 
+        /// <summary>
+        ///     Perform a right (clockwise) rotation.
+        /// </summary>
+        /// <param name="node">Node to rotate about.</param>
+        /// <returns>New node with rotation applied.</returns>
         private AVLTreeNode<TKey> RotateRight(AVLTreeNode<TKey> node)
         {
             var temp1 = node;
@@ -267,6 +391,12 @@ namespace DataStructures.AVLTree
             return node;
         }
 
+        /// <summary>
+        ///     Helper function to get node instance with minimum key value
+        ///     in the specified subtree.
+        /// </summary>
+        /// <param name="node">Node specifying root of subtree.</param>
+        /// <returns>Minimum value in node's subtree.</returns>
         private AVLTreeNode<TKey> GetMin(AVLTreeNode<TKey> node)
         {
             while (node.Left is not null)
@@ -277,6 +407,12 @@ namespace DataStructures.AVLTree
             return node;
         }
 
+        /// <summary>
+        ///     Helper function to get node instance with maximum key value
+        ///     in the specified subtree.
+        /// </summary>
+        /// <param name="node">Node specifyng root of subtree.</param>
+        /// <returns>Maximum value in node's subtree.</returns>
         private AVLTreeNode<TKey> GetMax(AVLTreeNode<TKey> node)
         {
             while (node.Right is not null)
