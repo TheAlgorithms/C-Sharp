06f276c15c2780fe43784d059310150ca1553ea1
Aayush Borkar <aayushb14@gmail.com>
2021-10-14 14:58:02 +0530

    Fix errors in ModularExponentiation

--- Algorithms/Numeric/ModularExponentiation.cs
+++ Algorithms/Numeric/ModularExponentiation.cs
@@ -2,20 +2,20 @@ using System.Text;
 
 namespace Algorithms.Numeric
 {
-    /// <summary> 
+    /// <summary>
     ///     Modular exponentiation is a type of exponentiation performed over a modulus
     ///     Modular exponentiation c is: c = b^e mod m where b is base, e is exponent, m is modulus
-    ///     Wiki: https://en.wikipedia.org/wiki/Modular_exponentiation
+    ///     (Wiki: https://en.wikipedia.org/wiki/Modular_exponentiation).
     /// </summary>
     public class ModularExponentiation
     {
         /// <summary>
-        ///     Performs Modular Exponentiation on b, e, m
+        ///     Performs Modular Exponentiation on b, e, m.
         /// </summary>
-        /// <param name=b>Base</param>
-        /// <param name=e>Exponent</param>
-        /// <param name=m>Modulus</param>
-        /// <returns>Modular Exponential</returns>
+        /// <param name="b">Base.</param>
+        /// <param name="e">Exponent.</param>
+        /// <param name="m">Modulus.</param>
+        /// <returns>Modular Exponential.</returns>
         public int ModularPow(int b, int e, int m)
         {
             // initialize result in variable res
@@ -25,15 +25,18 @@ namespace Algorithms.Numeric
                 // 1 divides every number
                 return 0;
             }
+
             if (m < 0)
             {
                 // exponential not defined in this case
                 return -1;
             }
+
             for (int i = 0; i < e; i++)
             {
                 res = (res * b) % m;
             }
+
             return res;
         }
     }
