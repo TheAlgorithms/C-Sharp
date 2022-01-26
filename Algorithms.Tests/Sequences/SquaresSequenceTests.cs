0d1f17e6f190f6239020835b9c4e53f395ef20ab
Oleksii Herasymenko <oherasy@softserveinc.com>
2022-01-26 02:21:28 +0200

    Add A000290 Squares sequence

---
+++ Algorithms.Tests/Sequences/SquaresSequenceTests.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using System.Numerics;
+using Algorithms.Sequences;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Algorithms.Tests.Sequences
+{
+    public class SquaresSequenceTests
+    {
+        [Test]
+        public void First10ElementsCorrect()
+        {
+            var sequence = new SquaresSequence().Sequence.Take(10);
+            sequence.SequenceEqual(new BigInteger[] { 0, 1, 4, 9, 16, 25, 36, 49, 64, 81 })
+                .Should().BeTrue();
+        }
+    }
+}
---
+++ Algorithms/Sequences/SquaresSequence.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Numerics;
+
+namespace Algorithms.Sequences
+{
+    /// <summary>
+    ///     <para>
+    ///         Sequence of square numbers.
+    ///     </para>
+    ///     <para>
+    ///         Wikipedia: https://wikipedia.org/wiki/Square_number.
+    ///     </para>
+    ///     <para>
+    ///         OEIS: http://oeis.org/A000290.
+    ///     </para>
+    /// </summary>
+    public class SquaresSequence : ISequence
+    {
+        /// <summary>
+        /// Gets sequence of square numbers.
+        /// </summary>
+        public IEnumerable<BigInteger> Sequence
+        {
+            get
+            {
+                var n = new BigInteger(0);
+
+                while (true)
+                {
+                    yield return n * n;
+                    n++;
+                }
+            }
+        }
+    }
+}
