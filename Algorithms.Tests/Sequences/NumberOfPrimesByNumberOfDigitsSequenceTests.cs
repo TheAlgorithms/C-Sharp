993e24ccc0cf67348165423945be7ec198789c77
Oleksii Herasymenko <oherasy@softserveinc.com>
2022-02-09 02:05:55 +0200

    Add A006879 Number of Primes by Number of Digits sequence

---
+++ Algorithms.Tests/Sequences/NumberOfPrimesByNumberOfDigitsSequenceTests.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using System.Numerics;
+using Algorithms.Sequences;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Algorithms.Tests.Sequences
+{
+    public class NumberOfPrimesByNumberOfDigitsSequenceTests
+    {
+        [Test]
+        public void First5ElementsCorrect()
+        {
+            var sequence = new NumberOfPrimesByNumberOfDigitsSequence().Sequence.Take(5);
+            sequence.SequenceEqual(new BigInteger[] { 0, 4, 21, 143, 1061 })
+                .Should().BeTrue();
+        }
+    }
+}
---
+++ Algorithms/Sequences/NumberOfPrimesByNumberOfDigitsSequence.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+
+namespace Algorithms.Sequences
+{
+    /// <summary>
+    ///     <para>
+    ///         Number of primes with n digits
+    ///         (The number of primes between 10^(n-1) and 10^n).
+    ///     </para>
+    ///     <para>
+    ///         Wikipedia: https://wikipedia.org/wiki/Prime-counting_function.
+    ///     </para>
+    ///     <para>
+    ///         OEIS: https://oeis.org/A006879.
+    ///     </para>
+    /// </summary>
+    public class NumberOfPrimesByNumberOfDigitsSequence : ISequence
+    {
+        /// <summary>
+        /// Gets sequence of number of primes.
+        /// </summary>
+        public IEnumerable<BigInteger> Sequence
+        {
+            get
+            {
+                ISequence primes = new PrimesSequence();
+                var prevPowerOf10 = new BigInteger(0);
+                var powerOf10 = new BigInteger(1);
+
+                while (true)
+                {
+                    yield return primes.Sequence.
+                        SkipWhile(p => p < prevPowerOf10).TakeWhile(p => p < powerOf10).Count();
+
+                    prevPowerOf10 = powerOf10;
+                    powerOf10 *= 10;
+                }
+            }
+        }
+    }
+}
