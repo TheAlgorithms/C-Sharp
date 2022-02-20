6e642d2c792ac7a0f975af0a5cb267ad2108ec99
Oleksii Herasymenko <oherasy@softserveinc.com>
2022-02-21 01:52:20 +0200

    Add A006862 Euclid Numbers sequence

---
+++ Algorithms.Tests/Sequences/EuclidNumbersSequenceTests.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using System.Numerics;
+using Algorithms.Sequences;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Algorithms.Tests.Sequences
+{
+    public class EuclidNumbersSequenceTests
+    {
+        [Test]
+        public void First10ElementsCorrect()
+        {
+            var sequence = new EuclidNumbersSequence().Sequence.Take(10);
+            sequence.SequenceEqual(new BigInteger[] 
+                { 2, 3, 7, 31, 211, 2311, 30031, 510511, 9699691, 223092871 })
+                .Should().BeTrue();
+        }
+    }
+}
