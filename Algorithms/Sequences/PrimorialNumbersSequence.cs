a3cb5ae1fb0bb2e329265708104e912b11811d92
Oleksii Herasymenko <oherasy@softserveinc.com>
2022-02-21 01:41:47 +0200

    Add A002110 Primorial Numbers sequence

---
+++ Algorithms/Sequences/PrimorialNumbersSequence.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace Algorithms.Sequences
+{
+    /// <summary>
+    ///     <para>
+    ///         Sequence of primorial numbers: product of first n primes.
+    ///     </para>
+    ///     <para>
+    ///         Wikipedia: https://wikipedia.org/wiki/Primorial.
+    ///     </para>
+    ///     <para>
+    ///         OEIS: https://oeis.org/A002110.
+    ///     </para>
+    /// </summary>
+    public class PrimorialNumbersSequence : ISequence
+    {
+        /// <summary>
+        /// Gets sequence of primorial numbers.
+        /// </summary>
+        public IEnumerable<BigInteger> Sequence
+        {
+            get
+            {
+                var primes = new PrimesSequence().Sequence;
+                var n = new BigInteger(1);
+
+                foreach (var p in primes)
+                {
+                    yield return n;
+                    n *= p;
+                }
+            }
+        }
+    }
+}
