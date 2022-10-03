c6df77fee37b03f578eb52fef69cd614ff0b2ecb
Richard Vasquez <1706965+RichardVasquez@users.noreply.github.com>
2022-10-03 00:50:46 -0700

    Base POC and initial passing test from OEIS data.
    Need to document, and create more detailed test(s).

---
+++ Algorithms.Tests/Sequences/OnesCountingSequenceTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using System.Numerics;
+using Algorithms.Sequences;
+using NUnit.Framework;
+
+namespace Algorithms.Tests.Sequences;
+
+[TestFixture]
+public class OnesCountingSequenceTest
+{
+    [Test]
+    public void Count105()
+    {
+        var values = new BigInteger[] {
+                                          0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4, 1, 2, 2, 3, 2, 3, 3, 4,
+                                          2, 3, 3, 4, 3, 4, 4, 5, 1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5, 2,
+                                          3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6, 1, 2, 2, 3, 2, 3, 3, 4, 2, 3,
+                                          3, 4, 3, 4, 4, 5, 2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6, 2, 3, 3,
+                                          4, 3, 4, 4, 5, 3,
+                                      };
+        var sequence = new OnesCountingSequence().Sequence.Take(values.Length);
+    }
+}
---
+++ Algorithms/Sequences/OnesCountingSequence.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Numerics;
+
+namespace Algorithms.Sequences;
+
+public class OnesCountingSequence : ISequence
+{
+    public IEnumerable<BigInteger> Sequence
+    {
+        get
+        {
+            yield return 0;
+            var depth = 0;
+            while (true)
+            {
+                foreach (var count in GenerateFractalCount(BigInteger.One, depth))
+                {
+                    yield return count;
+                }
+
+                depth++;
+            }
+        }
+    }
+
+    private static IEnumerable<BigInteger> GenerateFractalCount(BigInteger i, int depth)
+    {
+        if (depth == 0)
+        {
+            yield return i;
+        }
+
+        foreach (var half1 in GenerateFractalCount(i, depth - 1))
+        {
+            yield return half1;
+        }
+
+        foreach (var half2 in GenerateFractalCount(i + 1, depth - 1))
+        {
+            yield return half2;
+        }
+    }
+}
