e223597e878d393d0347620a87f9a89b59ea4684
Konstantinos Kalafatis <kalafatiskwstas@gmail.com>
2022-10-03 08:59:09 +0300

    Fix pull request suggestions

--- Algorithms.Tests/Numeric/KrishnamurthyNumberCheckerTests.cs
+++ Algorithms.Tests/Numeric/KrishnamurthyNumberCheckerTests.cs
@@ -3,7 +3,6 @@ using NUnit.Framework;
 
 namespace Algorithms.Tests.Numeric
 {
-    [TestFixture]
     public class KrishnamurthyNumberCheckerTests
     {
         [Test]
--- Algorithms/Numeric/KrishnamurthyNumberChecker.cs
+++ Algorithms/Numeric/KrishnamurthyNumberChecker.cs
@@ -27,22 +27,12 @@ namespace Algorithms.Numeric
 
             while (n != 0)
             {
-                int factorial = Factorial(n % 10);
+                int factorial = (int)Factorial.Calculate(n % 10);
                 sumOfFactorials += factorial;
                 n = n / 10;
             }
 
             return tmp == sumOfFactorials;
-        }
-
-        private static int Factorial(int n)
-        {
-            if (n < 0)
-            {
-                throw new ArgumentException($"Expected non-negative number. Actual number: {n}");
-            }
-
-            return (n == 0 || n == 1) ? 1 : n * Factorial(n - 1);
         }
     }
 }
