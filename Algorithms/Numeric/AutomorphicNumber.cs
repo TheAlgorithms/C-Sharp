68296c27203094c8faa1104c1628d4ae6c5ce87d
Konstantinos Kalafatis <kalafatiskwstas@gmail.com>
2022-10-06 14:32:40 +0300

    Add Automorphic Number calculator implementation

---
+++ Algorithms/Numeric/AutomorphicNumber.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace Algorithms.Numeric
+{
+    /// <summary>
+    /// Calculates Automorphic numbers. A number is said to be an Automorphic number
+    /// if the square of the number will contain the number itself at the end.
+    /// </summary>
+    public static class AutomorphicNumber
+    {
+        /// <summary>
+        /// Generates a list of automorphic numbers that are between <paramref name="lowerBound"/> and <paramref name="upperBound"/>
+        /// inclusive.
+        /// </summary>
+        /// <param name="lowerBound">The lower bound of the list.</param>
+        /// <param name="upperBound">The upper bound of the list.</param>
+        /// <returns>A list that contains all of the automorphic numbers between <paramref name="lowerBound"/> and <paramref name="upperBound"/> inclusive.</returns>
+        /// <exception cref="ArgumentException">If the <paramref name="lowerBound"/> or <paramref name="upperBound"/> is not greater than zero.</exception>
+        /// <remarks>If the <paramref name="lowerBound"/> is greater than the <paramref name="upperBound"/> then the two bounds are flipped.</remarks>
+        public static List<long> GetAutomorphicNumbers(long lowerBound, long upperBound)
+        {
+            if (lowerBound < 1)
+            {
+                throw new ArgumentException($"Lower Bound must be greater than 0: Actual value {lowerBound}");
+            }
+
+            if (upperBound < 1)
+            {
+                throw new ArgumentException($"Upper Bound must be greater than 0: Actual value {upperBound}");
+            }
+
+            if (lowerBound > upperBound)
+            {
+                var tmp = lowerBound;
+                lowerBound = upperBound;
+                upperBound = tmp;
+            }
+
+            List<long> result = new();
+
+            for (long i = lowerBound; i <= upperBound; i++)
+            {
+                if (IsAutomorphic(i))
+                {
+                    result.Add(i);
+                }
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Checks if a given natural number is automorphic or not.
+        /// </summary>
+        /// <param name="number">The number to check.</param>
+        /// <returns>True if the number is automorphic, false otherwise.</returns>
+        /// <exception cref="ArgumentException">If the number is non-positive.</exception>
+        public static bool IsAutomorphic(long number)
+        {
+            if (number < 1)
+            {
+                throw new ArgumentException($"An automorphic number must always be positive: Actual value {number}");
+            }
+
+            BigInteger square = new BigInteger(number * number);
+
+            // Extract the last digits of both numbers
+            while (number > 0)
+            {
+                if (number % 10 != square % 10)
+                {
+                    return false;
+                }
+
+                number /= 10;
+                square /= 10;
+            }
+
+            return true;
+        }
+    }
+}
