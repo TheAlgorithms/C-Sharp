1c1bf980d193bb4970b57598f950a400506ff9b0
Wrrikk <86593125+Wrrikk@users.noreply.github.com>
2022-10-07 09:18:44 +0530

    BoyerMooreTests.cs

---
+++ Algorithms.Tests/Search/BoyerMooreTests.cs
@@ -0,0 +1,36 @@
+using Algorithms.Search;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Algorithms.Tests.Search
+{
+    [TestClass]
+    public class BoyerMoore_Tests
+    {
+        [TestMethod]
+        public void BoyerMoore_Majority_Finder_Test()
+        {
+            var elementCount = 1000;
+
+            var rnd = new Random();
+            var randomNumbers = new List<int>();
+
+            while (randomNumbers.Count < elementCount / 2)
+            {
+                randomNumbers.Add(rnd.Next(0, elementCount));
+            }
+
+            var majorityElement = rnd.Next(0, elementCount);
+
+            randomNumbers.AddRange(Enumerable.Repeat(majorityElement, elementCount / 2 + 1));
+            randomNumbers = randomNumbers.OrderBy(x => rnd.Next()).ToList();
+
+            var expected = majorityElement;
+            var actual = BoyerMoore<int>.FindMajority(randomNumbers);
+
+            Assert.AreEqual(actual, expected);
+        }
+    }
+}
