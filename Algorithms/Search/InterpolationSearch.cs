88bc790e1dee1193560d836ecde9bb9c7ccf3e1b
PtrStruct <va.riley@outlook.com>
2022-10-15 12:31:29 +0200

    Refactored it to be more readable as well as usable.

--- Algorithms/Search/InterpolationSearch.cs
+++ Algorithms/Search/InterpolationSearch.cs
@@ -1,48 +1,61 @@
-﻿namespace Algorithms.Search;
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithms.Search;
 
 /// <summary>
-/// An interpolation search requires a sorted array in order for it to work.
+/// Interpolation search is an algorithm used for searching for a key in an array that has been ordered by numerical values.
+/// It's similar to a BinarySearch in the form of it truncating the array of which is searching though.
+/// There are a few things that the array needs to keep track of while running.
+/// L = The left most index of the array.
+/// R = The right most index of the array.
+/// Key = The value we're trying to find.
+/// It utilizes a formula which runs every iteration until it finds the key we're looking for.
+/// Formula: L + ((R-L) / (values[R] - values[L])) * (key - values[L]).
 /// </summary>
-public static class InterpolationSearch
+public class InterpolationSearch
 {
-    private static int[] values = { 1, 3, 5, 8, 10, 22, 31, 35, 37, 42, 51 };
+    /* Index of values to search from */
+    private readonly int[] indicies = { 1, 3, 5, 8, 10, 22, 31, 35, 37, 42, 51 };
 
-    /* L = Left most index
-     * R = Right most index
-     * Key = Value to find
-     */
+    /* The key (value) we want to find in the index of values */
+    private readonly int key = 22;
 
-    /* Calculate an estimated index */
-    /* Formula: L + ((R-L) / (values[R] - values[L])) * (key - values[L]) */
-    /* Formula: 0 + ((10-0) / (51 - 1)) * (22 - 1) */
-
-    public static int Search()
+    private int Search(IReadOnlyList<int> valuesToSearch, int keyToFind)
     {
-        var key = 22;
         var l = 0;
-        var r = values.Length - 1;
+        var r = valuesToSearch.Count - 1;
 
-        while (key >= values[l] && key <= values[r] && l <= r)
+        while (keyToFind >= valuesToSearch[l] && keyToFind <= valuesToSearch[r] && l <= r)
         {
-            int seek = l + (r - l) * (key - values[l]) / (values[r] - values[l]);
-            if (values[seek] == key)
+            /* Perform the formula in order to get the estimate index of where the Key might be. */
+            int seek = l + (r - l) * (keyToFind - valuesToSearch[l]) / (valuesToSearch[r] - valuesToSearch[l]);
+            /* If the value was found, we'll return the value. */
+            if (valuesToSearch[seek] == keyToFind)
             {
                 return seek;
             }
 
-            /* If the seek value is less than the key,
-             Then we want to truncate the left side of the array. */
-            if (values[seek] < key)
+            /* If we did not find the value within a certain iteration then we need to truncate the array.*/
+            if (valuesToSearch[seek] < keyToFind)
             {
+                /* If the seek value is less than the key, Then we want to truncate the left side of the array. */
                 l = seek + 1;
             }
             else
             {
+                /* If the seek value is greater than than the key, Then we want to truncate the right side of the array. */
                 r = seek - 1;
             }
         }
 
         /* Return -1 in order to indicate that the key was not found. */
         return -1;
+    }
+
+    protected InterpolationSearch()
+    {
+        var value = Search(indicies, key);
+        Console.WriteLine(value != -1 ? $"Value [{value}]: was found!" : $"[Key {key}] was not found.");
     }
 }
