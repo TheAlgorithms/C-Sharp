139ae5efc7db341f98ef7a351457dfbe378c2e23
OCTIPI <tfbmec@gmail.com>
2022-10-23 15:13:11 +0200

    Added missing test case

--- Algorithms.Tests/Other/GregorianEasterTest.cs
+++ Algorithms.Tests/Other/GregorianEasterTest.cs
@@ -11,12 +11,18 @@ namespace Algorithms.Tests.Other
         [TestCase(1991, "31/03/1991")]
         [TestCase(1954, "18/04/1954")]
         [TestCase(2000, "23/04/2000")]
-
         public static void CalculateTest(int year, string expected)
         {
             DateTime result = GregorianEaster.Calculate(year);
 
             Assert.AreEqual(result, Convert.ToDateTime(expected));
+        }
+
+        [Test]
+        [TestCase(100)]
+        public static void CalculateInvalidYearTest(int year)
+        {
+            Assert.Throws(Is.TypeOf<ArgumentException>().And.Message.EqualTo("Invalid year"), delegate { GregorianEaster.Calculate(year); });
         }
     }
 }
