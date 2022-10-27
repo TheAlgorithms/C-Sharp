73f1461b81820ed16440463dda59eb8d4a08f591
Damian Sikora <dsikora85@gmail.com>
2022-10-27 21:04:03 +0200

    Added Pollards Rho algorithm

---
+++ Algorithms.Tests/Other/PollardsRhoFactorizingTests.cs
@@ -0,0 +1,19 @@
+using Algorithms.Other;
+using NUnit.Framework;
+
+namespace Algorithms.Tests.Other
+{
+    public class PollardsRhoFactorizingTests
+    {
+        [TestCase(8051, 97)]
+        [TestCase(105, 21)]
+        [TestCase(253, 11)]
+        [TestCase(10403, 101)]
+        [TestCase(187, 11)]
+        public void SimpleTest(int number, int expectedResult)
+        {
+            var result = PollardsRhoFactorizing.Calculate(number);
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
