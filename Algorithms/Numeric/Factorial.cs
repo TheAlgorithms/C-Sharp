740af69f6b773cba2a75b42b972bf12867d299dd
Blade Assault <bladeassault45@gmail.com>
2023-03-30 20:58:10 -0400

    Factorial for Big Numbers

--- Algorithms/Numeric/Factorial.cs
+++ Algorithms/Numeric/Factorial.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Numerics;
 
 namespace Algorithms.Numeric
 {
@@ -21,6 +22,36 @@ namespace Algorithms.Numeric
             }
 
             return num == 0 ? 1 : num * Calculate(num - 1);
+        }
+
+        /// <summary>
+        ///     Calculates factorial of a big number.
+        /// </summary>
+        /// <param name="num">Big Input number.</param>
+        /// <returns>Factorial of Big input number.</returns>
+        public static BigInteger Calculate(BigInteger num)
+        {
+            // Don't calculate factorial if input is a negative number.
+            if (BigInteger.Compare(num, BigInteger.Zero) < 0)
+            {
+                throw new ArgumentException("Only for num >= 0");
+            }
+
+            // Factorial of 0 is 1. No need to calculate further.
+            if(BigInteger.Compare(num, BigInteger.Zero) == 0)
+            {
+                return BigInteger.One;
+            }
+
+            // Factorial of numbers greater than 0.
+            BigInteger result = BigInteger.One;
+
+            for (BigInteger i = BigInteger.One; BigInteger.Compare(i, num) <= 0; i = BigInteger.Add(i, BigInteger.One))
+            {
+                result = BigInteger.Multiply(result, i);
+            }
+
+            return result;
         }
     }
 }
