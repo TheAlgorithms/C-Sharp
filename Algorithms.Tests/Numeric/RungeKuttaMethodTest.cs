83b544396c3d39a684a2dcd0ce2056ad4f996c40
BSzmolke <40246238+BSzmolke@users.noreply.github.com>
2023-05-20 11:42:31 +0200

    Add Runge-Kutta method (#394)

---
+++ Algorithms.Tests/Numeric/RungeKuttaMethodTest.cs
@@ -0,0 +1,43 @@
+using Algorithms.Numeric;
+using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace Algorithms.Tests.Numeric
+{
+    public static class RungeKuttaTest
+    {
+        [Test]
+        public static void TestLinearEquation()
+        {
+            Func<double, double, double> exampleEquation = (x, _) => x;
+            List<double[]> points = RungeKuttaMethod.ClassicRungeKuttaMethod(0, 4, 0.001, 0, exampleEquation);
+            var yEnd = points[^1][1];
+            yEnd.Should().BeApproximately(8, 0.01);
+        }
+
+        [Test]
+        public static void TestExampleFunciton()
+        {
+            Func<double, double, double> exampleEquation = (_, y) => y;
+            List<double[]> points = RungeKuttaMethod.ClassicRungeKuttaMethod(0, 4, 0.0125, 1, exampleEquation);
+            var yEnd = points[^1][1];
+            yEnd.Should().BeApproximately(54.598, 0.0005);
+        }
+
+        [Test]
+        public static void StepsizeIsZeroOrNegative_ThrowsArgumentOutOfRangeException()
+        {
+            Func<double, double, double> exampleEquation = (x, _) => x;
+            Assert.Throws<ArgumentOutOfRangeException>(() => RungeKuttaMethod.ClassicRungeKuttaMethod(0, 4, 0, 0, exampleEquation));
+        }
+
+        [Test]
+        public static void StartIsLargerThanEnd_ThrowsArgumentOutOfRangeException()
+        {
+            Func<double, double, double> exampleEquation = (x, _) => x;
+            Assert.Throws<ArgumentOutOfRangeException>(() => RungeKuttaMethod.ClassicRungeKuttaMethod(0, -4, 0.1, 0, exampleEquation));
+        }
+    }
+}
---
+++ Algorithms/Numeric/RungeKuttaMethod.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+
+namespace Algorithms.Numeric
+{
+    /// <summary>
+    ///     In numerical analysis, the Runge–Kutta methods are a family of implicit and explicit iterative methods,
+    ///     used in temporal discretization for the approximate solutions of simultaneous nonlinear equations.
+    ///     The most widely known member of the Runge–Kutta family is generally referred to as
+    ///     "RK4", the "classic Runge–Kutta method" or simply as "the Runge–Kutta method".
+    ///     </summary>
+    public static class RungeKuttaMethod
+    {
+        /// <summary>
+        ///     Loops through all the steps until xEnd is reached, adds a point for each step and then
+        ///     returns all the points.
+        /// </summary>
+        /// <param name="xStart">Initial conditions x-value.</param>
+        /// <param name="xEnd">Last x-value.</param>
+        /// <param name="stepSize">Step-size on the x-axis.</param>
+        /// <param name="yStart">Initial conditions y-value.</param>
+        /// <param name="function">The right hand side of the differential equation.</param>
+        /// <returns>The solution of the Cauchy problem.</returns>
+        public static List<double[]> ClassicRungeKuttaMethod(
+            double xStart,
+            double xEnd,
+            double stepSize,
+            double yStart,
+            Func<double, double, double> function)
+        {
+            if (xStart >= xEnd)
+            {
+                throw new ArgumentOutOfRangeException(
+                    nameof(xEnd),
+                    $"{nameof(xEnd)} should be greater than {nameof(xStart)}");
+            }
+
+            if (stepSize <= 0)
+            {
+                throw new ArgumentOutOfRangeException(
+                    nameof(stepSize),
+                    $"{nameof(stepSize)} should be greater than zero");
+            }
+
+            List<double[]> points = new();
+            double[] firstPoint = { xStart, yStart };
+            points.Add(firstPoint);
+
+            var yCurrent = yStart;
+            var xCurrent = xStart;
+
+            while (xCurrent < xEnd)
+            {
+                var k1 = function(xCurrent, yCurrent);
+                var k2 = function(xCurrent + 0.5 * stepSize, yCurrent + 0.5 * stepSize * k1);
+                var k3 = function(xCurrent + 0.5 * stepSize, yCurrent + 0.5 * stepSize * k2);
+                var k4 = function(xCurrent + stepSize, yCurrent + stepSize * k3);
+
+                yCurrent += (1.0 / 6.0) * stepSize * (k1 + 2 * k2 + 2 * k3 + k4);
+                xCurrent += stepSize;
+
+                double[] newPoint = { xCurrent, yCurrent };
+                points.Add(newPoint);
+            }
+
+            return points;
+        }
+    }
+}
