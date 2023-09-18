78a53173dc29ccb5f58b6b96f95511cbad3895df
7IronSnow7 <divan.debruin25@gmail.com>
2023-09-17 20:44:37 -0400

    Added a factorial method to Sequences repository

--- Algorithms/Sequences/FactorialTest.cs
+++ Algorithms/Sequences/FactorialTest.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +8,38 @@ namespace Algorithms.Sequences
 {
     internal class FactorialTest
     {
+        public static void Main(string[] args)
+        {
+            // Utilize n to be your input value.
+            var n = 0;
+            Console.WriteLine(Factorial(n));
+        }
+
+        /// <summary>
+        /// Factorial Method finds the nth factorial for the desired input number.
+        /// </summary>
+        public static string Factorial(int number)
+        {
+            var factorialNum = 1;
+            if (number > 0)
+            {
+                for (var i = 1; i <= number; i++)
+                {
+                    factorialNum *= i;
+                }
+
+                Console.Write($"Factorial of {number} is = ");
+            }
+            else if (number == 0)
+            {
+                return "Factorial of 0 = 1";
+            }
+            else
+            {
+                return "Factorial is not defined for a negative number.";
+            }
+
+            return factorialNum.ToString();
+        }
     }
 }
