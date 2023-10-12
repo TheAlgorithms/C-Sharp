d91770e7f74f1d47d77212ad21a422ef665401cc
Konstantinos Kalafatis <kalafatiskwstas@gmail.com>
2023-10-12 20:09:34 +0300

    Add implementation for Jaccard Similarity

---
+++ Algorithms/Strings/Similarity/JaccardSimilarity.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithms.Strings.Similarity;
+
+public class JaccardSimilarity
+{
+    public double Calculate(string left, string right)
+    {
+        if (left == null || right == null)
+        {
+            var paramName = left == null ? nameof(left) : nameof(right);
+            throw new ArgumentNullException(paramName, "Input cannot be null");
+        }
+
+        var leftLength = left.Length;
+        var rightLength = right.Length;
+
+        if (leftLength == 0 && rightLength == 0)
+        {
+            return 1.0d;
+        }
+
+        if (leftLength == 0 || rightLength == 0)
+        {
+            return 0.0d;
+        }
+
+        var leftSet = new HashSet<char>();
+        for (var i = 0; i < leftLength; i++)
+        {
+            leftSet.Add(left[i]);
+        }
+
+        var rightSet = new HashSet<char>();
+        for (var i = 0; i < rightLength; i++)
+        {
+            rightSet.Add(right[i]);
+        }
+
+        var unionSet = new HashSet<char>(leftSet);
+        foreach (var c in rightSet)
+        {
+            unionSet.Add(c);
+        }
+
+        var intersectionSize = leftSet.Count + rightSet.Count - unionSet.Count;
+        return 1.0d * intersectionSize / unionSet.Count;
+    }
+}
