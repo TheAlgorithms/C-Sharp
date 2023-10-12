a23f3cd62b2b5ca0dc77ce99fb8f18375099ca95
Konstantinos Kalafatis <kalafatiskwstas@gmail.com>
2023-10-12 09:09:26 +0300

    Refactor: Update MD2Digest Unit Tests and Address Parallelization Issue
    - Refactored the unit tests for MD2Digest to use the [TestCase] decorator.
    - Added the [NonParallelizable] directive to the class due to an issue with NUnit test parallelization.
    - Identified that parallelized tests were causing the internal arrays of the algorithm not to reset after digest completion.
    - This led to random hash generation due to incorrect state.
    - The [NonParallelizable] directive ensures tests are run sequentially, preventing this issue.

--- Algorithms.Tests/Crypto/Digests/Md2DigestTests.cs
+++ Algorithms.Tests/Crypto/Digests/Md2DigestTests.cs
@@ -6,44 +6,28 @@ using NUnit.Framework;
 
 namespace Algorithms.Tests.Crypto.Digesters;
 
+[NonParallelizable]
 public class Md2DigestTests
 {
     private readonly Md2Digest digest = new Md2Digest();
 
-    private readonly string[] messages =
+    [TestCase("", "8350E5A3E24C153DF2275C9F80692773")]
+    [TestCase("a", "32EC01EC4A6DAC72C0AB96FB34C0B5D1")]
+    [TestCase("abc", "DA853B0D3F88D99B30283A69E6DED6BB")]
+    [TestCase("message digest", "AB4F496BFB2A530B219FF33031FE06B0")]
+    [TestCase("abcdefghijklmnopqrstuvwxyz", "4E8DDFF3650292AB5A4108C3AA47940B")]
+    [TestCase("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789", "DA33DEF2A42DF13975352846C30338CD")]
+    [TestCase("12345678901234567890123456789012345678901234567890123456789012345678901234567890", "D5976F79D83D3A0DC9806C3C66F3EFD8")]
+    public void Digest_ReturnsCorrectValue_Dummy(string input, string expected)
     {
-        "",
-        "a",
-        "abc",
-        "message digest",
-        "abcdefghijklmnopqrstuvwxyz",
-        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789",
-        "12345678901234567890123456789012345678901234567890123456789012345678901234567890",
-    };
+        var inputBytes = Encoding.ASCII.GetBytes(input);
 
-    private readonly string[] expectedDigests =
-    {
-        "8350E5A3E24C153DF2275C9F80692773",
-        "32EC01EC4A6DAC72C0AB96FB34C0B5D1",
-        "DA853B0D3F88D99B30283A69E6DED6BB",
-        "AB4F496BFB2A530B219FF33031FE06B0",
-        "4E8DDFF3650292AB5A4108C3AA47940B",
-        "DA33DEF2A42DF13975352846C30338CD",
-        "D5976F79D83D3A0DC9806C3C66F3EFD8",
-    };
+        var result = digest.Digest(inputBytes);
 
-    [Test]
-    public void Digest_ReturnsCorrectValue()
-    {
-        for (var i = 0; i < messages.Length; i++)
-        {
-            var input = Encoding.ASCII.GetBytes(messages[i]);
+        var output = Convert.ToHexString(result);
 
-            var result = digest.Digest(input);
-
-            var output = Convert.ToHexString(result);
-
-            output.Should().Be(expectedDigests[i]);
-        }
+        output.Should().Be(expected);
     }
+
+
 }
