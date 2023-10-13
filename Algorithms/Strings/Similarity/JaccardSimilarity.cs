e21974a8d6724fef6510e3b616ea7f080b42f316
Konstantinos Kalafatis <kalafatiskwstas@gmail.com>
2023-10-13 20:02:30 +0300

    Fix documentation for JaccardSimilarity

--- Algorithms/Strings/Similarity/JaccardSimilarity.cs
+++ Algorithms/Strings/Similarity/JaccardSimilarity.cs
@@ -3,6 +3,34 @@ using System.Collections.Generic;
 
 namespace Algorithms.Strings.Similarity;
 
+/// <summary>
+///  <para>
+/// Jaccard similarity is a statistic that measures how similar two sets of data are. It is calculated by dividing
+/// the number of common elements in both sets by the number of elements in either set. More formally, it is the
+/// quotient of the division of the size of the size of the intersection divided by the size of the union of two sets.
+/// </para>
+/// <para>
+/// The result is a value between 0 and 1, where 0 means no similarity and 1 means perfect similarity.
+/// </para>
+/// <para>
+/// For example, suppose we have two sets of words:
+/// <list type="bullet">
+/// <item>
+/// A = {apple, banana, cherry, date}
+/// </item>
+/// <item>
+/// B = {banana, cherry, elderberry, fig}
+/// </item>
+/// </list>
+/// </para>
+/// <para>
+/// The number of common elements in both sets is 2 (banana and cherry). The number of elements in either set is 6
+/// (apple, banana, cherry, date, elderberry, fig).
+/// </para>
+/// <para>
+/// The Jaccard similarity coefficient is 2 / 6 = 0.333333 or 33.333% similarity.
+/// </para>
+/// </summary>
 public class JaccardSimilarity
 {
     /// <summary>
@@ -13,11 +41,7 @@ public class JaccardSimilarity
     /// <returns>A double value between 0 and 1 that represents the similarity of the two strings.</returns>
     /// <exception cref="ArgumentNullException">Thrown when either the input is null.</exception>
     /// <remarks>
-    /// The Jaccard similarity coefficient is defined as the size of the intersection divided by the size of the union
-    /// of two sets.
-    /// <para>
     /// This method uses a HashSet to represent the sets of characters in the input strings.
-    /// </para>
     /// </remarks>
     public double Calculate(string left, string right)
     {
