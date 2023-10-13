56ea389bbc3b6283e70bda672d13159ef6dd6485
Konstantinos Kalafatis <kalafatiskwstas@gmail.com>
2023-10-13 22:02:45 +0300

    Introduce a more appropriate exception for padding errors

---
+++ Algorithms/Crypto/Paddings/InvalidPaddingException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Algorithms.Crypto.Paddings;
+
+public class InvalidPaddingException : Exception
+{
+    public InvalidPaddingException()
+    {
+    }
+
+    public InvalidPaddingException(string? message)
+        : base(message)
+    {
+    }
+
+    public InvalidPaddingException(string? message, Exception? innerException)
+        : base(message, innerException)
+    {
+    }
+}
