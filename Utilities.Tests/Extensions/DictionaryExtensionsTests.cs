5ab5950a3c98ba826044ee542c737490d1151c81
Andrii Siriak <siryaka@gmail.com>
2024-02-04 22:03:28 +0200

    Update Utilities.Tests/Extensions/DictionaryExtensionsTests.cs

--- Utilities.Tests/Extensions/DictionaryExtensionsTests.cs
+++ Utilities.Tests/Extensions/DictionaryExtensionsTests.cs
@@ -32,6 +32,5 @@ public class DictionaryExtensionsTests
         dictionary.Should().ContainKey("one");
         dictionary.Should().ContainKey("two");
         dictionary.Should().ContainKey("three");
-
     }
 }
