962102979443aa5d43b6aabf0198bd7562a91bd5
RicardoRibeiroRodrigues <riywar@gmail.com>
2024-08-20 15:24:38 -0300

    Codacy Static Code Analysis fix

--- Algorithms.Tests/Numeric/AbsTests.cs
+++ Algorithms.Tests/Numeric/AbsTests.cs
@@ -20,9 +20,9 @@ public static class AbsTests
         Assert.That(result, Is.EqualTo(expected));
     }
 
-    [TestCase(new int[] { -3, -1, 2, -11 }, -11)]
-    [TestCase(new int[] { 0, 5, 1, 11 }, 11)]
-    [TestCase(new double[] { 3.0, -10.0, -2.0 }, -10.0d)]
+    [TestCase(new[] { -3, -1, 2, -11 }, -11)]
+    [TestCase(new[] { 0, 5, 1, 11 }, 11)]
+    [TestCase(new[] { 3.0, -10.0, -2.0 }, -10.0d)]
     public static void GetAbsMax<T>(T[] inputNums, T expected) where T : INumber<T>
     {
         // Act
@@ -32,9 +32,9 @@ public static class AbsTests
         Assert.That(result, Is.EqualTo(expected));
     }
 
-    [TestCase(new int[] { -3, -1, 2, -11 }, -1)]
-    [TestCase(new int[] { -3, -5, 1, -11 }, 1)]
-    [TestCase(new int[] { 0, 5, 1, 11 }, 0)]
+    [TestCase(new[] { -3, -1, 2, -11 }, -1)]
+    [TestCase(new[] { -3, -5, 1, -11 }, 1)]
+    [TestCase(new[] { 0, 5, 1, 11 }, 0)]
     public static void GetAbsMin<T>(T[] inputNums, T expected) where T : INumber<T>
     {
         // Act
