b5e5f5ff03dc4050e44ea288e5e788935616f49d
Konstantinos Kalafatis <kalafatiskwstas@gmail.com>
2024-08-21 09:59:55 +0300

    Add Optimal String Alignment Algorithm Implementation

---
+++ Algorithms.Tests/Strings/Similarity/OptimalStringAlignmentTests.cs
@@ -0,0 +1,94 @@
+using Algorithms.Strings.Similarity;
+using FluentAssertions;
+using NUnit.Framework;
+using System;
+
+namespace Algorithms.Tests.Strings.Similarity
+{
+    [TestFixture]
+    public class OptimalStringAlignmentTests
+    {
+        [Test]
+        public void Calculate_IdenticalStrings_ReturnsZero()
+        {
+            var result = OptimalStringAlignment.Calculate("example", "example");
+            result.Should().Be(0.0);
+        }
+
+        [Test]
+        public void Calculate_FirstStringEmpty_ReturnsLengthOfSecondString()
+        {
+            var result = OptimalStringAlignment.Calculate("", "example");
+            result.Should().Be("example".Length);
+        }
+
+        [Test]
+        public void Calculate_SecondStringEmpty_ReturnsLengthOfFirstString()
+        {
+            var result = OptimalStringAlignment.Calculate("example", "");
+            result.Should().Be("example".Length);
+        }
+
+        [Test]
+        public void Calculate_BothStringsEmpty_ReturnsZero()
+        {
+            var result = OptimalStringAlignment.Calculate("", "");
+            result.Should().Be(0.0);
+        }
+
+        [Test]
+        public void Calculate_OneInsertion_ReturnsOne()
+        {
+            var result = OptimalStringAlignment.Calculate("example", "examples");
+            result.Should().Be(1.0);
+        }
+
+        [Test]
+        public void Calculate_OneDeletion_ReturnsOne()
+        {
+            var result = OptimalStringAlignment.Calculate("examples", "example");
+            result.Should().Be(1.0);
+        }
+
+        [Test]
+        public void Calculate_OneSubstitution_ReturnsOne()
+        {
+            var result = OptimalStringAlignment.Calculate("example", "exbmple");
+            result.Should().Be(1.0);
+        }
+
+        [Test]
+        public void Calculate_OneTransposition_ReturnsOne()
+        {
+            var result = OptimalStringAlignment.Calculate("example", "exmaple");
+            result.Should().Be(1.0);
+        }
+
+        [Test]
+        public void Calculate_MultipleOperations_ReturnsCorrectDistance()
+        {
+            var result = OptimalStringAlignment.Calculate("kitten", "sitting");
+            result.Should().Be(3.0);
+        }
+
+        [Test]
+        public void Calculate_FirstStringNull_ThrowsArgumentNullException()
+        {
+            string? nullString = null;
+#pragma warning disable CS8604 // Possible null reference argument.
+            Action act = () => OptimalStringAlignment.Calculate(nullString, "example");
+#pragma warning restore CS8604 // Possible null reference argument.
+            act.Should().Throw<ArgumentNullException>().WithMessage("*firstString*");
+        }
+
+        [Test]
+        public void Calculate_SecondStringNull_ThrowsArgumentNullException()
+        {
+            string? nullString = null;
+#pragma warning disable CS8604 // Possible null reference argument.
+            Action act = () => OptimalStringAlignment.Calculate("example", nullString);
+#pragma warning restore CS8604 // Possible null reference argument.
+            act.Should().Throw<ArgumentNullException>().WithMessage("*secondString*");
+        }
+    }
+}
