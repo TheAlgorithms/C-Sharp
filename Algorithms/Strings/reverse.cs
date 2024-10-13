6432a32fa2694ce92fe87a3d313a35e11e093b63
Tatiparthi Chenchu Jahnavi <85797034+Jahnavi998@users.noreply.github.com>
2024-10-13 19:48:47 +0530

    Update reverse.cs

--- Algorithms/Strings/reverse.cs
+++ Algorithms/Strings/reverse.cs
@@ -1,34 +1,34 @@
 using System;
+using System.Text;
 
 namespace Algorithms.Strings;
 
 public static class ReversedString
 {
     //comment
-    // In this Main Method we are taking the input and calling ReverseString Method which will return reversedString and we will print in main Method
+    //In this Main Method we are taking the input and calling ReverseString Method which will return reversedString and we will print in main Method
     //comment
     public static void Main(string[] args)
     {
         // Sample Input : "reverse"
         // Sample Output : "esrever"
         string input= Console.ReadLine();
-        string reversedString = ReverseString(input.Trim());
-        Console.WriteLine(reversedString);
+        ReverseString(input.Trim());
     }
     
     //comment
-    // This Method will take input as string and return the reversed string
+    //This Method will take input as string and return the reversed string
     //comment
     
-    public static string ReverseString (string input)
+    public static void ReverseString (string input)
     {
-        string reverseString="";
+        StringBuilder reverseString= new StringBuilder();
         int length = input.Length;
         while(length!=0)
         {
-            reverseString= reverseString+input[length-1];
+            reverseString.Append(input[length-1]);
             length-=1;
         }
-        return reverseString;
+        Console.WriteLine(reverseString);
     }
 }
