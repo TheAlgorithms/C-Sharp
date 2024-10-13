cff1bc31ece79cfa5a54be2ebacaa678ace83597
Tatiparthi Chenchu Jahnavi <85797034+Jahnavi998@users.noreply.github.com>
2024-10-13 19:30:04 +0530

    Create reverse.cs
    written logic to reverse a string

---
+++ Algorithms/Strings/reverse.cs
@@ -0,0 +1,34 @@
+using System;
+
+namespace Algorithms.Strings;
+
+public static class ReversedString
+{
+    //comment
+    // In this Main Method we are taking the input and calling ReverseString Method which will return reversedString and we will print in main Method
+    //comment
+    public static void Main(string[] args)
+    {
+        // Sample Input : "reverse"
+        // Sample Output : "esrever"
+        string input= Console.ReadLine();
+        string reversedString = ReverseString(input.Trim());
+        Console.WriteLine(reversedString);
+    }
+    
+    //comment
+    // This Method will take input as string and return the reversed string
+    //comment
+    
+    public static string ReverseString (string input)
+    {
+        string reverseString="";
+        int length = input.Length;
+        while(length!=0)
+        {
+            reverseString= reverseString+input[length-1];
+            length-=1;
+        }
+        return reverseString;
+    }
+}
