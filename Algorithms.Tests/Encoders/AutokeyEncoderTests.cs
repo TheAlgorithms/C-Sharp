8fec0422cc740e016c211f8ccaced83738f063c1
Jc Calayag <138150245+Jccqt@users.noreply.github.com>
2025-07-12 01:54:16 +0800

    Add Autokey encoder (#509)

---
+++ Algorithms.Tests/Encoders/AutokeyEncoderTests.cs
@@ -0,0 +1,26 @@
+using System;
+using Algorithms.Encoders;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace Algorithms.Tests.Encoders
+{
+    public static class AutokeyEncoderTests
+    {
+        [Test]
+        public static void DecodedStringIsTheSame()
+        {
+            // Arrange
+            var plainText = "PLAINTEXT";
+            var keyword = "KEYWORD";
+            var encoder = new AutokeyEncorder();
+
+            // Act
+            var encoded = encoder.Encode(plainText, keyword);
+            var decoded = encoder.Decode(encoded, keyword);
+
+            // Assert
+            Assert.That(decoded, Is.EqualTo(plainText));
+        }
+    }
+}
