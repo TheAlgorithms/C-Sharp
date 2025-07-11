6c6ecb9359ff4902b2f1f2ecfba07313f805f852
Jc Calayag <138150245+Jccqt@users.noreply.github.com>
2025-07-11 14:20:21 +0800

    Add Gnome sort algorithm

---
+++ Algorithms.Tests/Sorters/Comparison/GnomeSorterTests.cs
@@ -0,0 +1,27 @@
+using System;
+using Algorithms.Sorters.Comparison;
+using Algorithms.Tests.Helpers;
+using NUnit.Framework;
+
+namespace Algorithms.Tests.Sorters.Comparison;
+
+public static class GnomeSorterTests
+{
+    [Test]
+    public static void ArraySorted(
+        [Random(0, 1000, 100, Distinct = true)]
+        int n)
+    {
+        // Arrange
+        var sorter = new GnomeSorter<int>();
+        var intComparer = new IntComparer();
+        var (correctArray, testArray) = RandomHelper.GetArrays(n);
+
+        // Act
+        sorter.Sort(testArray, intComparer);
+        Array.Sort(correctArray, intComparer);
+
+        // Assert
+        Assert.That(correctArray, Is.EqualTo(testArray));
+    }
+}
---
+++ Algorithms/Sorters/Comparison/GnomeSorter.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+
+namespace Algorithms.Sorters.Comparison;
+
+/// <summary>
+///     Class that implements gnome sort algorithm.
+/// </summary>
+/// <typeparam name="T">Type of array element.</typeparam>
+public class GnomeSorter<T> : IComparisonSorter<T>
+{
+    /// <summary>
+    ///     Moves forward through the array until it founds two elements out of order,
+    ///     then swaps them and move back one position,
+    ///     internal, in-place, stable,
+    ///     time complexity: O(n2),
+    ///     space complexity: O(1).
+    /// </summary>
+    /// <param name="array">Array to sort.</param>
+    /// <param name="comparer">Compares elements.</param>
+    public void Sort(T[] array, IComparer<T> comparer)
+    {
+        int index = 0;
+
+        while(index < array.Length)
+        {
+            if(index == 0 || comparer.Compare(array[index], array[index - 1]) >= 0)
+            {
+                index++;
+            }
+            else
+            {
+                Swap(array, index, index - 1);
+                index--;
+            }
+        }
+    }
+
+    public void Swap(T[] array, int index1, int index2)
+    {
+        (array[index1], array[index2]) = (array[index2], array[index1]);
+    }
+}
