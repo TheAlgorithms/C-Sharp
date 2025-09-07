1b9aa7e90660b187dae7edc1624d74fc34d2beb0
Karan Chadha <72665232+KaranChadha10@users.noreply.github.com>
2025-09-07 14:08:34 +0530

    Migrate to file-scoped namespaces and remove unused global usings (#521)

--- Algorithms/Other/Geohash.cs
+++ Algorithms/Other/Geohash.cs
@@ -1,84 +1,79 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
-namespace Algorithms.Other
+namespace Algorithms.Other;
+
+public static class Geohash
 {
-    public static class Geohash
+    private const string Base32Characters = "0123456789bcdefghjkmnpqrstuvwxyz"; // Convert latitude and longitude coordinates into a concise string
+    private const int GeohashLength = 12; // ± 1.86 cm
+
+    /// <summary>
+    /// Encodes the provided latitude and longitude coordinates into a Geohash string.
+    /// Geohashing is a method to encode geographic coordinates (latitude, longitude).
+    /// into a short string of letters and digits. Each character in the resulting Geohash .
+    /// string adds more precision to the location. The longer the Geohash, the smaller the area.
+    /// </summary>
+    /// <param name="latitude">The latitude of the location to encode. It must be a value between -90 and 90.</param>
+    /// <param name="longitude">The longitude of the location to encode. It must be a value between -180 and 180.</param>
+    /// <returns>
+    /// A Geohash string of length 12 representing the location with high precision.
+    /// A longer Geohash provides higher precision in terms of geographic area.
+    /// and a 12-character Geohash can be accurate down to around 1.86 cm.
+    /// </returns>
+    public static string Encode(double latitude, double longitude)
     {
-        private const string Base32Characters = "0123456789bcdefghjkmnpqrstuvwxyz"; // Convert latitude and longitude coordinates into a concise string
-        private const int GeohashLength = 12; // ± 1.86 cm
+        double[] latitudeRange = new[] { -90.0, 90.0 };
+        double[] longitudeRange = new[] { -180.0, 180.0 };
+        bool isEncodingLongitude = true;
+        int currentBit = 0;
+        int base32Index = 0;
+        StringBuilder geohashResult = new StringBuilder();
 
-        /// <summary>
-        /// Encodes the provided latitude and longitude coordinates into a Geohash string.
-        /// Geohashing is a method to encode geographic coordinates (latitude, longitude).
-        /// into a short string of letters and digits. Each character in the resulting Geohash .
-        /// string adds more precision to the location. The longer the Geohash, the smaller the area.
-        /// </summary>
-        /// <param name="latitude">The latitude of the location to encode. It must be a value between -90 and 90.</param>
-        /// <param name="longitude">The longitude of the location to encode. It must be a value between -180 and 180.</param>
-        /// <returns>
-        /// A Geohash string of length 12 representing the location with high precision.
-        /// A longer Geohash provides higher precision in terms of geographic area.
-        /// and a 12-character Geohash can be accurate down to around 1.86 cm.
-        /// </returns>
-        public static string Encode(double latitude, double longitude)
+        while (geohashResult.Length < GeohashLength)
         {
-            double[] latitudeRange = new[] { -90.0, 90.0 };
-            double[] longitudeRange = new[] { -180.0, 180.0 };
-            bool isEncodingLongitude = true;
-            int currentBit = 0;
-            int base32Index = 0;
-            StringBuilder geohashResult = new StringBuilder();
+            double midpoint;
 
-            while (geohashResult.Length < GeohashLength)
+            if (isEncodingLongitude)
             {
-                double midpoint;
-
-                if (isEncodingLongitude)
+                midpoint = (longitudeRange[0] + longitudeRange[1]) / 2;
+                if (longitude > midpoint)
                 {
-                    midpoint = (longitudeRange[0] + longitudeRange[1]) / 2;
-                    if (longitude > midpoint)
-                    {
-                        base32Index |= 1 << (4 - currentBit);
-                        longitudeRange[0] = midpoint;
-                    }
-                    else
-                    {
-                        longitudeRange[1] = midpoint;
-                    }
+                    base32Index |= 1 << (4 - currentBit);
+                    longitudeRange[0] = midpoint;
                 }
                 else
                 {
-                    midpoint = (latitudeRange[0] + latitudeRange[1]) / 2;
-                    if (latitude > midpoint)
-                    {
-                        base32Index |= 1 << (4 - currentBit);
-                        latitudeRange[0] = midpoint;
-                    }
-                    else
-                    {
-                        latitudeRange[1] = midpoint;
-                    }
+                    longitudeRange[1] = midpoint;
                 }
-
-                isEncodingLongitude = !isEncodingLongitude;
-
-                if (currentBit < 4)
+            }
+            else
+            {
+                midpoint = (latitudeRange[0] + latitudeRange[1]) / 2;
+                if (latitude > midpoint)
                 {
-                    currentBit++;
+                    base32Index |= 1 << (4 - currentBit);
+                    latitudeRange[0] = midpoint;
                 }
                 else
                 {
-                    geohashResult.Append(Base32Characters[base32Index]);
-                    currentBit = 0;
-                    base32Index = 0;
+                    latitudeRange[1] = midpoint;
                 }
             }
 
-            return geohashResult.ToString();
+            isEncodingLongitude = !isEncodingLongitude;
+
+            if (currentBit < 4)
+            {
+                currentBit++;
+            }
+            else
+            {
+                geohashResult.Append(Base32Characters[base32Index]);
+                currentBit = 0;
+                base32Index = 0;
+            }
         }
+
+        return geohashResult.ToString();
     }
 }
--- Algorithms/Stack/ReverseStack.cs
+++ Algorithms/Stack/ReverseStack.cs
@@ -1,44 +1,43 @@
 using System;
 using System.Collections.Generic;
 
-namespace Algorithms.Stack
+namespace Algorithms.Stack;
+
+/// <summary>
+///     Reverses the elements in a stack using recursion.
+/// @author Mohit Singh. <a href="https://github.com/mohit-gogitter">mohit-gogitter</a>
+/// </summary>
+public class ReverseStack
 {
     /// <summary>
-    ///     Reverses the elements in a stack using recursion.
-    /// @author Mohit Singh. <a href="https://github.com/mohit-gogitter">mohit-gogitter</a>
+    /// Recursively reverses the elements of the specified stack.
     /// </summary>
-    public class ReverseStack
+    /// <typeparam name="T">The type of elements in the stack.</typeparam>
+    /// <param name="stack">The stack to be reversed. This parameter cannot be null.</param>
+    /// <exception cref="ArgumentNullException">Thrown when the stack parameter is null.</exception>
+    public void Reverse<T>(Stack<T> stack)
     {
-        /// <summary>
-        /// Recursively reverses the elements of the specified stack.
-        /// </summary>
-        /// <typeparam name="T">The type of elements in the stack.</typeparam>
-        /// <param name="stack">The stack to be reversed. This parameter cannot be null.</param>
-        /// <exception cref="ArgumentNullException">Thrown when the stack parameter is null.</exception>
-        public void Reverse<T>(Stack<T> stack)
+        if (stack.Count == 0)
         {
-            if (stack.Count == 0)
-            {
-                return;
-            }
-
-            T temp = stack.Pop();
-            Reverse(stack);
-            InsertAtBottom(stack, temp);
+            return;
         }
 
-        private void InsertAtBottom<T>(Stack<T> stack, T value)
+        T temp = stack.Pop();
+        Reverse(stack);
+        InsertAtBottom(stack, temp);
+    }
+
+    private void InsertAtBottom<T>(Stack<T> stack, T value)
+    {
+        if (stack.Count == 0)
         {
-            if (stack.Count == 0)
-            {
-                stack.Push(value);
-            }
-            else
-            {
-                T temp = stack.Pop();
-                InsertAtBottom(stack, value);
-                stack.Push(temp);
-            }
+            stack.Push(value);
+        }
+        else
+        {
+            T temp = stack.Pop();
+            InsertAtBottom(stack, value);
+            stack.Push(temp);
         }
     }
 }
